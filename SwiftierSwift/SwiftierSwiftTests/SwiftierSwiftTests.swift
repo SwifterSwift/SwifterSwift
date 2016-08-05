37055906c488d17eb460b6c5eb04b1af79521420
Omar Albeik <omaralbeik@gmail.com>
2016-08-05 19:01:28 +0300

    refactor: Xcode project created

---
+++ SwiftierSwift/SwiftierSwiftTests/SwiftierSwiftTests.swift
@@ -0,0 +1,24 @@
+//
+//  SwiftierSwiftTests.swift
+//  SwiftierSwiftTests
+//
+//  Created by Omar Albeik on 8/5/16.
+//  Copyright © 2016 Omar Albeik. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftierSwift
+
+class SwiftierSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+}
