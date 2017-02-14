3f2ec115823202e3ab26aa90948fc8d717e6f087
Steven Deutsch <stevensdeutsch@yahoo.com>
2017-02-13 19:40:11 -0600

    Add UIAlertControllerExtensionsTests

---
+++ Tests/SwifterSwiftTests/UIKitExtensionsTests/UIAlertControllerExtensionsTests.swift
@@ -0,0 +1,88 @@
+//
+//  UIAlertControllerExtensionsTests.swift
+//  SwifterSwift
+//
+//  Created by Steven on 2/13/17.
+//  Copyright © 2017 omaralbeik. All rights reserved.
+//
+
+import XCTest
+@testable import SwifterSwift
+
+class UIAlertControllerExtensionsTests: XCTestCase {
+    
+    func testAddAction() {
+        
+        let alertController = UIAlertController(title: "Title", message: "Message", preferredStyle: .alert)
+        let discardedResult = alertController.addAction(title: "ActionTitle", style: .destructive, isEnabled: false, handler: nil)
+        
+        XCTAssertNotNil(discardedResult)
+        
+        XCTAssertTrue(alertController.actions.count == 1)
+        
+        let action = alertController.actions.first
+        
+        XCTAssertEqual(action?.title, "ActionTitle")
+        XCTAssertEqual(action?.style, .destructive)
+        XCTAssertEqual(action?.isEnabled, false)
+    }
+    
+    func testSelector() {}
+    
+    func testAddTextField() {
+        
+        let alertController = UIAlertController(title: "Title", message: "Message", preferredStyle: .alert)
+        
+        let selector = #selector(UIAlertControllerExtensionsTests.testSelector)
+        
+        alertController.addTextField(text: "TextField", placeholder: "PlaceHolder", editingChangedTarget: self, editingChangedSelector: selector)
+        
+        XCTAssertTrue(alertController.textFields?.count == 1)
+        
+        let textField = alertController.textFields?.first
+        
+        XCTAssertEqual(textField?.text, "TextField")
+        XCTAssertEqual(textField?.placeholder, "PlaceHolder")
+        XCTAssertEqual(textField?.allTargets.first, self)
+        XCTAssertNotNil(textField?.actions(forTarget: self, forControlEvent: .editingChanged))
+    }
+    
+    func testMessageInit() {
+        
+        let alertController = UIAlertController(title: "Title", message: "Message", defaultActionButtonTitle: "Ok", tintColor: .blue)
+        
+        XCTAssertNotNil(alertController)
+        
+        XCTAssertEqual(alertController.title, "Title")
+        XCTAssertEqual(alertController.message, "Message")
+        XCTAssertEqual(alertController.view.tintColor, .blue)
+        
+        XCTAssertTrue(alertController.actions.count == 1)
+        
+        let defaultAction = alertController.actions.first
+        
+        XCTAssertEqual(defaultAction?.title, "Ok")
+        XCTAssertEqual(defaultAction?.style, .default)
+    }
+    
+    func testErrorInit() {
+        
+        enum TestError: Error { case error }
+        let error = TestError.error
+        
+        let alertController = UIAlertController(title: "Title", error: error, defaultActionButtonTitle: "Ok", tintColor: .red)
+        
+        XCTAssertNotNil(alertController)
+        
+        XCTAssertEqual(alertController.title, "Title")
+        XCTAssertEqual(alertController.message, error.localizedDescription)
+        XCTAssertEqual(alertController.view.tintColor, .red)
+        
+        XCTAssertTrue(alertController.actions.count == 1)
+        
+        let defaultAction = alertController.actions.first
+        
+        XCTAssertEqual(defaultAction?.title, "Ok")
+        XCTAssertEqual(defaultAction?.style, .default)
+    }
+}
