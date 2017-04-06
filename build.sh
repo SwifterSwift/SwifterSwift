a4930949992134ae8ef49aa62ef6e94cd38a1c22
Omar Albeik <omaralbeik@gmail.com>
2017-04-06 09:51:17 -0700

    Downgraded osx_image to xcode 8.2 to fix codecov

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ rm -rf $DERIVED_DATA &&
 time xcodebuild clean test \
     -project SwifterSwift.xcodeproj \
     -scheme 'SwifterSwift tvOS' \
-    -sdk appletvsimulator10.2 \
+    -sdk appletvsimulator10.1 \
     -derivedDataPath $DERIVED_DATA \
     -destination 'platform=tvOS Simulator,name=Apple TV 1080p' \
     OTHER_SWIFT_FLAGS='-Xfrontend -debug-time-function-bodies' \
@@ -30,9 +30,9 @@ rm -rf $DERIVED_DATA &&
 time xcodebuild clean test \
     -project SwifterSwift.xcodeproj \
     -scheme 'SwifterSwift iOS' \
-    -sdk iphonesimulator10.3 \
+    -sdk iphonesimulator10.2 \
     -derivedDataPath $DERIVED_DATA \
-    -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.3' \
+    -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.2' \
     OTHER_SWIFT_FLAGS='-Xfrontend -debug-time-function-bodies' \
     | tee build.log \
     | xcpretty &&
