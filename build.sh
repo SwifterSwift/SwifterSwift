0e00c8dd8ef467871d9f7e4294d1a79a0e7d415c
Omar Albeik <omaralbeik@gmail.com>
2017-07-10 15:55:55 +0300

    Updated build.sh

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ rm -rf $DERIVED_DATA &&
 time xcodebuild clean test \
     -project SwifterSwift.xcodeproj \
     -scheme 'SwifterSwift macOS' \
+    -sdk macosx10.11 \
     -derivedDataPath $DERIVED_DATA \
     | tee build.log \
     | xcpretty &&
@@ -17,7 +18,7 @@ rm -rf $DERIVED_DATA &&
 time xcodebuild clean test \
     -project SwifterSwift.xcodeproj \
     -scheme 'SwifterSwift tvOS' \
-    -sdk appletvsimulator10.2 \
+    -sdk appletvsimulator9.2 \
     -derivedDataPath $DERIVED_DATA \
     -destination 'platform=tvOS Simulator,name=Apple TV 1080p' \
     | tee build.log \
@@ -27,9 +28,9 @@ rm -rf $DERIVED_DATA &&
 time xcodebuild clean test \
     -project SwifterSwift.xcodeproj \
     -scheme 'SwifterSwift iOS' \
-    -sdk iphonesimulator10.3 \
+    -sdk iphonesimulator9.3 \
     -derivedDataPath $DERIVED_DATA \
-    -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.3' \
+    -destination 'platform=iOS Simulator,name=iPhone 7,OS=9.3' \
     | tee build.log \
     | xcpretty &&
 cat build.log
