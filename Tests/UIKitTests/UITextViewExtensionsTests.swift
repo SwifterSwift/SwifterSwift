1a8eee6d06b277c35ae42d42e79d8966655ea4f3
Omar Albeik <omaralbeik@gmail.com>
2017-07-21 09:24:30 +0300

    Fixed UITextViewExtensionsTests :sweat_smile:

--- Tests/UIKitTests/UITextViewExtensionsTests.swift
+++ Tests/UIKitTests/UITextViewExtensionsTests.swift
@@ -26,12 +26,16 @@ class UITextViewExtensionsTests: XCTestCase {
 	}
 	
 	func testScroll() {
-		let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+		let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation."
 		textView.text = text
 		textView.scrollToBottom()
 		XCTAssertNotEqual(textView.contentOffset.y, 0.0)
-		XCTAssertEqual(textView.contentOffset.y, 336.0)
-		
+		#if os(iOS)
+			XCTAssertEqual(textView.contentOffset.y, 87.0)
+		#elseif os(tvOS)
+			XCTAssertEqual(textView.contentOffset.y, 3370.0)
+		#endif
+			
 		textView.scrollToTop()
 		XCTAssertNotEqual(textView.contentOffset.y, 0.0)
 		
