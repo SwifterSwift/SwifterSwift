33237964def1c2e8e8acfaa56b3aac37b833ccc4
Max Härtwig <vyaxxx@gmail.com>
2017-10-03 20:19:55 +0200

    Add convenience initializer init(text:) to UILabel, including a test

--- Sources/Extensions/UIKit/UILabelExtensions.swift
+++ Sources/Extensions/UIKit/UILabelExtensions.swift
@@ -12,6 +12,12 @@ import UIKit
 // MARK: - Methods
 public extension UILabel {
 	
+    /// SwifterSwift: Initialize a UILabel with text
+    public convenience init(text: String?) {
+        self.init()
+        self.text = text
+    }
+    
 	/// SwifterSwift: Required height for a label
 	public var requiredHeight: CGFloat {
 		let label = UILabel(frame: CGRect(x: 0, y: 0, width: frame.width, height: CGFloat.greatestFiniteMagnitude))
