706dbcc914b160262b59694c3d469913dadcf6bf
Luciano <passos.luciano@outlook.com>
2017-10-06 22:42:45 -0300

    Correcting a mistake, the inconsistence was in the docs not implementation.

--- Sources/Extensions/SwiftStdlib/ArrayExtensions.swift
+++ Sources/Extensions/SwiftStdlib/ArrayExtensions.swift
@@ -503,17 +503,15 @@ public extension Array where Element: Equatable {
 	
 	/// SwifterSwift: Return array with all duplicate elements removed.
 	///
-	///		[1, 2, 2, 3, 4, 5, 5].duplicatesRemoved() -> [ 2, 5]
-	///		["h", "e", "l", "l", "o"]. duplicatesRemoved() -> ["l"]
-	///
+    ///     [1, 1, 2, 2, 3, 3, 3, 4, 5].duplicatesRemoved() -> [1, 2, 3, 4, 5])
+    ///     ["h", "e", "l", "l", "o"].duplicatesRemoved() -> ["h", "e", "l", "o"])
+    ///
 	/// - Returns: an array of unique elements.
+    ///
 	public func duplicatesRemoved() -> [Element] {
 		// Thanks to https://github.com/sairamkotha for improving the property
-        var noDuplicates: [Element] = []
         return reduce(into: [Element]()) {
-            if !noDuplicates.contains($1) {
-                noDuplicates.append($1)
-            } else {
+            if !$0.contains($1) {
                 $0.append($1)
             }
         }
