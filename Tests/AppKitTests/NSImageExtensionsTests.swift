0cefc4c3a5bd6381d99981046fe189e755f3d59a
Oleksandr Yakubchyk <buddax2@gmail.com>
2017-10-20 23:55:52 +0300

    Add scaled(toMaxSize:NSSize) to NSImage, including a test

---
+++ Tests/AppKitTests/NSImageExtensionsTests.swift
@@ -0,0 +1,32 @@
+//
+//  NSImageExtensionsTests.swift
+//  SwifterSwift-macOSTests
+//
+//  Created by BUDDAx2 on 20.10.2017.
+//
+
+#if os(macOS)
+    
+import XCTest
+@testable import SwifterSwift
+
+final class NSImageExtensionsTests: XCTestCase {
+    
+    func testScaledToHeight() {
+        let bundle = Bundle.init(for: NSImageExtensionsTests.self)
+        let image = bundle.image(forResource: NSImage.Name(rawValue: "TestImage"))
+        XCTAssertNotNil(image)
+        
+        let scaledImage = image!.scaled(toMaxSize: NSSize(width: 150, height: 150))
+        XCTAssertNotNil(scaledImage)
+        if scaledImage.size.width > scaledImage.size.height {
+            XCTAssertEqual(scaledImage.size.width, 150)
+        }
+        else {
+            XCTAssertEqual(scaledImage.size.height, 150)
+        }
+    }
+    
+}
+
+#endif
