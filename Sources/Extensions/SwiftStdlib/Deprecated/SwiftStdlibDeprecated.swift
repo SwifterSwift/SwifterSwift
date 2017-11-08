5dffbdcaa500529afe14055c773f0c9c16ffdbba
Luciano <passos.luciano@outlook.com>
2017-11-08 20:13:16 -0200

    Changing deprecation version :)

--- Sources/Extensions/SwiftStdlib/Deprecated/SwiftStdlibDeprecated.swift
+++ Sources/Extensions/SwiftStdlib/Deprecated/SwiftStdlibDeprecated.swift
@@ -10,7 +10,7 @@ extension String {
     ///
     ///        "Hello world!".length -> 12
     ///
-    @available(*, deprecated: 4.0.2, message: "This extension is deprecated, please use .count instead.")
+    @available(*, deprecated: 4.1.0, message: "This extension is deprecated, please use .count instead.")
     public var length: Int {
         return count
     }
@@ -21,7 +21,7 @@ extension String {
     ///
     /// - Parameter i: string index the slicing should start from.
     /// - Returns: sliced substring starting from start index (if applicable) (example: "Hello world".slicing(at: 6) -> "world")
-    @available(*, deprecated: 4.0.2, message: "Please use string[safe: i] instead.")
+    @available(*, deprecated: 4.1.0, message: "Please use string[safe: i] instead.")
     public func slicing(at i: Int) -> String? {
         guard i < count else {
             return nil
@@ -37,7 +37,7 @@ extension String {
     ///   - start: string index the slicing should start from.
     ///   - end: string index the slicing should end at.
     /// - Returns: sliced substring starting from start index, and ends at end index (if applicable) (example: "Hello World".slicing(from: 6, to: 11) -> "World")
-    @available(*, deprecated: 4.0.2, message: "Please use string[safe: start..<end] instead.")
+    @available(*, deprecated: 4.1.0, message: "Please use string[safe: start..<end] instead.")
     public func slicing(from start: Int, to end: Int) -> String? {
         guard end >= start else {
             return nil
@@ -52,7 +52,7 @@ extension String {
     ///
     /// - Parameter string: substring to search for.
     /// - Returns: first index of substring in string (if applicable).
-    @available(*, deprecated: 4.0.2, message: "Please use string.index(of: Character) or string.range(of: StringProtocol) instead.")
+    @available(*, deprecated: 4.1.0, message: "Please use string.index(of: Character) or string.range(of: StringProtocol) instead.")
     public func firstIndex(of string: String) -> Int? {
         return map({ String($0) }).index(of: string)
     }
@@ -65,7 +65,7 @@ extension String {
     ///
     /// - Parameter separator: separator to split string by.
     /// - Returns: array of strings separated by given string.
-    @available(*, deprecated: 4.0.2, message: "Please use string.split(separator: Character) instead.")
+    @available(*, deprecated: 4.1.0, message: "Please use string.split(separator: Character) instead.")
     public func splitted(by separator: Character) -> [String] {
         return split { $0 == separator }.map(String.init)
     }
@@ -75,7 +75,7 @@ extension String {
     ///        "foo".reversed() -> "oof"
     ///
     /// - Returns: The reversed string.
-    @available(*, deprecated: 4.0.2, message: "Please use String(str.reversed()) or reversed() -> ReversedCollection<String> instead.")
+    @available(*, deprecated: 4.1.0, message: "Please use String(str.reversed()) or reversed() -> ReversedCollection<String> instead.")
     public func reversed() -> String {
         let chars: [Character] = reversed()
         return String(chars)
