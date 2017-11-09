1ccf430253b1adf849138c8933399717b475c166
chaithanyaprathyush <prathyush.chaithanya@gmail.com>
2017-11-09 19:45:27 +0530

    Update CalendarExtensions.swift

--- Sources/Extensions/Foundation/CalendarExtensions.swift
+++ Sources/Extensions/Foundation/CalendarExtensions.swift
@@ -8,16 +8,16 @@
 
 import Foundation
 
+// MARK: - Methods
 public extension Calendar {
     /// SwifterSwift: Return the number of days in the month for a specified 'Date'.
     ///
-    /// let date = Date() // "Jan 12, 2017, 7:07 PM"
-    /// Calendar.current.numberOfDaysInMonth(for: date) -> 31
+    ///         let date = Date() // "Jan 12, 2017, 7:07 PM"
+    ///         Calendar.current.numberOfDaysInMonth(for: date) -> 31
     ///
-    /// - Parameters:
-    /// - for: the date form which the number of days in month is calculated.
+    /// - Parameter date: the date form which the number of days in month is calculated.
     /// - Returns: The number of days in the month of 'Date'.
-    func numberOfDaysInMonth(for date: Date) -> Int {
-        return self.range(of: .day, in: .month, for: date)?.count ?? 0
+    public func numberOfDaysInMonth(for date: Date) -> Int {
+        return range(of: .day, in: .month, for: date)?.count ?? 0
     }
 }
