db5af71d10dbbf282108b16a2aa19da9610a1107
Omar Albeik <omaralbeik@gmail.com>
2017-11-22 13:41:02 +0300

    Add write() method to NSImage extensions

--- Sources/Extensions/AppKit/NSImageExtensions.swift
+++ Sources/Extensions/AppKit/NSImageExtensions.swift
@@ -49,6 +49,22 @@ extension NSImage {
         // Return the new image
         return imageWithNewSize
     }
+	
+	/// SwifterSwift: Write NSImage to url.
+	///
+	/// - Parameters:
+	///   - url: Desired file URL.
+	///   - type: Type of image (default is .jpeg).
+	///   - compressionFactor: used only for JPEG files. The value is a float between 0.0 and 1.0, with 1.0 resulting in no compression and 0.0 resulting in the maximum compression possible.
+	public func write(to url: URL, fileType type: NSBitmapImageRep.FileType = .jpeg, compressionFactor: NSNumber = 1.0) {
+		// https://stackoverflow.com/a/45042611/3882644
+		
+		guard let data = tiffRepresentation else { return }
+		guard let imageRep = NSBitmapImageRep(data: data) else { return }
+		
+		guard let imageData = imageRep.representation(using: type, properties: [.compressionFactor : compressionFactor]) else { return }
+		try? imageData.write(to: url)
+	}
 }
 
 #endif
