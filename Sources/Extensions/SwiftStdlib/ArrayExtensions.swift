65f87a79fa940d60b7c30f7b1b0f39e853d084f6
Najdan Tomić <najdan.tomic@gmail.com>
2018-01-04 13:19:18 +0100

    Renamed indexes(of:) to indices(of:) in ArrayExtension (#355)

--- Sources/Extensions/SwiftStdlib/ArrayExtensions.swift
+++ Sources/Extensions/SwiftStdlib/ArrayExtensions.swift
@@ -496,22 +496,22 @@ public extension Array where Element: Equatable {
 		}
 		return found
 	}
-	
-	/// SwifterSwift: All indexes of specified item.
-	///
-	///		[1, 2, 2, 3, 4, 2, 5].indexes(of 2) -> [1, 2, 5]
-	///		[1.2, 2.3, 4.5, 3.4, 4.5].indexes(of 2.3) -> [1]
-	///		["h", "e", "l", "l", "o"].indexes(of "l") -> [2, 3]
-	///
-	/// - Parameter item: item to check.
-	/// - Returns: an array with all indexes of the given item.
-	public func indexes(of item: Element) -> [Int] {
-		var indexes: [Int] = []
-		for index in startIndex..<endIndex where self[index] == item {
-			indexes.append(index)
-		}
-		return indexes
-	}
+    
+    /// SwifterSwift: All indices of specified item.
+    ///
+    ///        [1, 2, 2, 3, 4, 2, 5].indices(of 2) -> [1, 2, 5]
+    ///        [1.2, 2.3, 4.5, 3.4, 4.5].indices(of 2.3) -> [1]
+    ///        ["h", "e", "l", "l", "o"].indices(of "l") -> [2, 3]
+    ///
+    /// - Parameter item: item to check.
+    /// - Returns: an array with all indices of the given item.
+    public func indices(of item: Element) -> [Int] {
+        var indices: [Int] = []
+        for index in startIndex..<endIndex where self[index] == item {
+            indices.append(index)
+        }
+        return indices
+    }
 	
 	/// SwifterSwift: Remove all instances of an item from array.
 	///
