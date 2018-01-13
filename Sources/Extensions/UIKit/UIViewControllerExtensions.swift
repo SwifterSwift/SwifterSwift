143b2285f68923b8b6df2b121b76867524e23901
Ratikanta Patra <ratikanta.patra1026@gmail.com>
2018-01-14 01:39:02 +0530

    Fixed lint issues

--- Sources/Extensions/UIKit/UIViewControllerExtensions.swift
+++ Sources/Extensions/UIKit/UIViewControllerExtensions.swift
@@ -52,7 +52,7 @@ public extension UIViewController {
     ///   - buttonTitles: (Optional)list of button titles for the alert. Default button i.e "OK" will be shown if this paramter is nil
     ///   - highlightedButtonIndex: (Optional) index of the button from buttonTitles that should be highlighted. If this parameter is nil no button will be highlighted
     ///   - completion: (Optional) completion block to be invoked when any one of the buttons is tapped. It passes the index of the tapped button as an argument
-    @discardableResult public func showAlert(title: String?, message: String?, buttonTitles: [String]? = nil, highlightedButtonIndex: Int? = nil, completion: ((Int) -> ())? = nil) -> UIAlertController {
+    @discardableResult public func showAlert(title: String?, message: String?, buttonTitles: [String]? = nil, highlightedButtonIndex: Int? = nil, completion: ((Int) -> Void)? = nil) -> UIAlertController {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         var allButtons = buttonTitles ?? [String]()
         if allButtons.count == 0 {
@@ -61,7 +61,7 @@ public extension UIViewController {
         
         for index in 0..<allButtons.count {
             let buttonTitle = allButtons[index]
-            let action = UIAlertAction(title: buttonTitle, style: .default, handler: { (action) in
+            let action = UIAlertAction(title: buttonTitle, style: .default, handler: { (_) in
                 completion?(index)
             })
             alertController.addAction(action)
