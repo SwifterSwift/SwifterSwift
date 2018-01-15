c443c608ba1f330caa3ebddc819c663c169e3078
Ratikanta Patra <ratikanta.patra1026@gmail.com>
2018-01-15 21:15:26 +0530

    Review comments incorporated

--- Sources/Extensions/UIKit/UIViewControllerExtensions.swift
+++ Sources/Extensions/UIKit/UIViewControllerExtensions.swift
@@ -52,6 +52,7 @@ public extension UIViewController {
     ///   - buttonTitles: (Optional)list of button titles for the alert. Default button i.e "OK" will be shown if this paramter is nil
     ///   - highlightedButtonIndex: (Optional) index of the button from buttonTitles that should be highlighted. If this parameter is nil no button will be highlighted
     ///   - completion: (Optional) completion block to be invoked when any one of the buttons is tapped. It passes the index of the tapped button as an argument
+    /// - Returns: UIAlertController object (discardable).
     @discardableResult public func showAlert(title: String?, message: String?, buttonTitles: [String]? = nil, highlightedButtonIndex: Int? = nil, completion: ((Int) -> Void)? = nil) -> UIAlertController {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         var allButtons = buttonTitles ?? [String]()
