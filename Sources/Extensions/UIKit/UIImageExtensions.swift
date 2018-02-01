2610fb815de076f6f5c4159b5b801ac011fdbf55
Max Härtwig <vyaxxx@gmail.com>
2018-02-01 06:29:11 +0100

    Add withRoundedCorners(radius:) extension to UIImage (#380)

--- Sources/Extensions/UIKit/UIImageExtensions.swift
+++ Sources/Extensions/UIKit/UIImageExtensions.swift
@@ -137,6 +137,31 @@ public extension UIImage {
 		UIGraphicsEndImageContext()
 		return tintedImage!
 	}
+    
+    /// SwifterSwift: UIImage with rounded corners
+    ///
+    /// - Parameters:
+    ///   - radius: corner radius (optional), resulting image will be round if unspecified
+    /// - Returns: UIImage with all corners rounded
+    public func withRoundedCorners(radius: CGFloat? = nil) -> UIImage? {
+        let maxRadius = min(size.width, size.height) / 2
+        let cornerRadius: CGFloat
+        if let radius = radius, radius > 0 && radius <= maxRadius {
+            cornerRadius = radius
+        } else {
+            cornerRadius = maxRadius
+        }
+        
+        UIGraphicsBeginImageContextWithOptions(size, false, scale)
+        
+        let rect = CGRect(origin: .zero, size: size)
+        UIBezierPath(roundedRect: rect, cornerRadius: cornerRadius).addClip()
+        draw(in: rect)
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
 	
 }
 
