2da16d98aa9b71ce496b99161e30a3267bdaf50f
Jason Jon Carreos <jason@ingenuity.ph>
2018-02-08 08:55:23 +0800

    UserDefaults extension to save codable object (#388)
    * UserDefaults extension to save codable object
    
    * Updates based on PR comments
    
    * Added custom encoder/decoder as argument to set: and object: methods
    
    * Separated tests for get and set codable object

--- Sources/Extensions/Foundation/UserDefaultsExtensions.swift
+++ Sources/Extensions/Foundation/UserDefaultsExtensions.swift
@@ -38,4 +38,33 @@ public extension UserDefaults {
 		return object(forKey: key) as? Date
 	}
 	
+    /// SwifterSwift: Retrieves a Codable object from UserDefaults.
+    ///
+    /// - Parameters:
+    ///   - type: Class that conforms to the Codable protocol.
+    ///   - key: Identifier of the object.
+    ///   - decoder: Custom JSONDecoder instance. Defaults to `JSONDecoder()`.
+    /// - Returns: Codable object for key (if exists).
+    public func object<T: Codable>(_ type: T.Type,
+                                   with key: String,
+                                   usingDecoder decoder: JSONDecoder = JSONDecoder()) -> T? {
+        guard let data = self.value(forKey: key) as? Data else { return nil }
+        
+        return try? decoder.decode(type.self, from: data)
+    }
+    
+    /// SwifterSwift: Allows storing of Codable objects to UserDefaults.
+    ///
+    /// - Parameters:
+    ///   - object: Codable object to store.
+    ///   - key: Identifier of the object.
+    ///   - encoder: Custom JSONEncoder instance. Defaults to `JSONEncoder()`.
+    public func set<T: Codable>(object: T,
+                                forKey key: String,
+                                usingEncoder encoder: JSONEncoder = JSONEncoder()) {
+        let data = try? encoder.encode(object)
+        
+        self.set(data, forKey: key)
+    }
+    
 }
