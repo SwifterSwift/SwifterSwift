a3d404324917b000f8cf84f1f501ccfd88c40635
n0an <nantog@gmail.com>
2018-04-04 11:52:17 +0300

    Collection extension methods all(), none(), all() moved to newly created Sequence Extension
    SequenceExtensions.swift file added
    SequenceExtensionsTests.swift file added

---
+++ Sources/Extensions/SwiftStdlib/SequenceExtensions.swift
@@ -0,0 +1,44 @@
+//
+//  SequenceExtensions.swift
+//  SwifterSwift
+//
+//  Created by Anton Novoselov on 04/04/2018.
+//  Copyright © 2018 SwifterSwift
+//
+
+import Foundation
+
+public extension Sequence {
+    /// SwifterSwift: Check if all elements in collection match a conditon.
+    ///
+    ///        [2, 2, 4].all(matching: {$0 % 2 == 0}) -> true
+    ///        [1,2, 2, 4].all(matching: {$0 % 2 == 0}) -> false
+    ///
+    /// - Parameter condition: condition to evaluate each element against.
+    /// - Returns: true when all elements in the array match the specified condition.
+    public func all(matching condition: (Element) throws -> Bool) rethrows -> Bool {
+        return try !contains { try !condition($0) }
+    }
+    
+    /// SwifterSwift: Check if no elements in collection match a conditon.
+    ///
+    ///        [2, 2, 4].none(matching: {$0 % 2 == 0}) -> false
+    ///        [1, 3, 5, 7].none(matching: {$0 % 2 == 0}) -> true
+    ///
+    /// - Parameter condition: condition to evaluate each element against.
+    /// - Returns: true when no elements in the array match the specified condition.
+    public func none(matching condition: (Element) throws -> Bool) rethrows -> Bool {
+        return try !contains { try condition($0) }
+    }
+    
+    /// SwifterSwift: Check if any element in collection match a conditon.
+    ///
+    ///        [2, 2, 4].any(matching: {$0 % 2 == 0}) -> false
+    ///        [1, 3, 5, 7].any(matching: {$0 % 2 == 0}) -> true
+    ///
+    /// - Parameter condition: condition to evaluate each element against.
+    /// - Returns: true when no elements in the array match the specified condition.
+    public func any(matching condition: (Element) throws -> Bool) rethrows -> Bool {
+        return try contains { try condition($0) }
+    }
+}
--- Tests/SwiftStdlibTests/CollectionExtensionsTests.swift
+++ Tests/SwiftStdlibTests/CollectionExtensionsTests.swift
@@ -30,19 +30,4 @@ final class CollectionExtensionsTests: XCTestCase {
         XCTAssert([1, 2, 3].contains([1, 2, 3].randomItem!))
         XCTAssertNil([].randomItem)
     }
-    
-    func testAllMatch() {
-        let collection = [2, 4, 6, 8, 10, 12]
-        XCTAssert(collection.all { $0 % 2 == 0 })
-    }
-    
-    func testAnyMatch() {
-        let collection = [3, 5, 8, 9, 11, 13]
-        XCTAssert(collection.any { $0 % 2 == 0 })
-    }
-    
-    func testNoneMatch() {
-        let collection = [3, 5, 7, 9, 11, 13]
-        XCTAssert(collection.none { $0 % 2 == 0 })
-    }
 }
