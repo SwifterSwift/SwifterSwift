cb522d75786a5db08c6410642e437828c9d4dcec
Seto Elkahfi <setoelkahfi@gmail.com>
2018-04-12 22:04:39 +0700

    Increase test coverage

--- Tests/UIKitTests/UITableViewExtensionsTests.swift
+++ Tests/UIKitTests/UITableViewExtensionsTests.swift
@@ -105,6 +105,14 @@ final class UITableViewExtensionsTests: XCTestCase {
     }
 
     func testSafeScrollToIndexPath() {
+        let validIndexPathTop = IndexPath(row: 0, section: 0)
+        XCTAssertNoThrow(tableView.safeScrollToRow(at: validIndexPathTop, at: .top, animated: false))
+        XCTAssertEqual(CGPoint.zero, tableView.contentOffset)
+        let validIndexPathBottom = IndexPath(row: 7, section: 1)
+        let bottomOffset = CGPoint(x: 0, y: tableView.contentSize.height - tableView.bounds.size.height)
+        tableView.safeScrollToRow(at: validIndexPathBottom, at: .bottom, animated: false)
+        XCTAssertEqual(bottomOffset.x, tableView.contentOffset.x, accuracy: 0.0)
+        XCTAssertEqual(bottomOffset.y, tableView.contentOffset.y, accuracy: 1.0)
         let invalidIndexPath = IndexPath(row: 10, section: 0)
         XCTAssertNoThrow(tableView.safeScrollToRow(at: invalidIndexPath, at: .bottom, animated: false))
     }
