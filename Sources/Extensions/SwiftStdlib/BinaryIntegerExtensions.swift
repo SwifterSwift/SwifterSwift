0174a0e39a654f4d3c2a89a6393f820c02a9a892
Guy Kogus <guy.kogus@gmail.com>
2018-04-13 15:22:12 +0200

    Further improve generic random number generator for all  base integer types. Create a new protocol type called `RandomizableInteger` that can use its Signed and Unsigned variants to generate un-modulo-biased random integers.

--- Sources/Extensions/SwiftStdlib/BinaryIntegerExtensions.swift
+++ Sources/Extensions/SwiftStdlib/BinaryIntegerExtensions.swift
@@ -7,19 +7,24 @@
 //
 
 public extension BinaryInteger {
+
     /// SwifterSwift: Generate a random integer within a given range.
-    ///
-    /// Note that if the distance between `lowerBound` and `upperBound` is greater than the type can represent,
-    /// e.g. `Int.min..<Int.max`,
-    /// the function will crash.
     ///
     /// - Parameters:
     ///   - lowerBound: The lower bound of the integer range. Defaults to 0.
     ///   - upperBound: The upper bound of the integer range.
     /// - Returns: An integer between `lowerBound` and `upperBound` (exclusive).
+    /// - Warning: This is an unsafe implementation.
+    ///
+    ///   It will crash for signed integers where `upperBound - lowerBound` has a value that can't be represented
+    ///   (e.g. `Int16.min..<Int16.max`).
+    ///
+    ///   It will crash for 64-bit integers, since it relies on `arc4random_uniform`, which provides a 32-bit random number.
     public static func random(lowerBound: Self = 0, upperBound: Self) -> Self {
         assert(lowerBound < upperBound)
-        let distance = Self(lowerBound.distance(to: upperBound))
+
+        let distance = upperBound - lowerBound
         return lowerBound + numericCast(arc4random_uniform(numericCast(distance)))
     }
+
 }
--- Tests/SwiftStdlibTests/BinaryIntegerExtensionsTests.swift
+++ Tests/SwiftStdlibTests/BinaryIntegerExtensionsTests.swift
@@ -11,60 +11,24 @@ import XCTest
 
 final class BinaryIntegerExtensionsTests: XCTestCase {
 
+    let testRuns = 10000
+
+    // MARK: Test helpers
+
+    func runBinaryIntegers<B: BinaryInteger>(lowerBound: B, upperBound: B) {
+        var values = Set<B>(minimumCapacity: testRuns)
+        for _ in 0..<testRuns {
+            let random = B.random(lowerBound: lowerBound, upperBound: upperBound)
+            XCTAssert(random >= lowerBound && random < upperBound)
+            values.insert(random)
+        }
+        XCTAssertEqual(B(values.count), upperBound - lowerBound)
+    }
+
+    // MARK: Test methods
+
     func testRandomInts() {
-        var values = Set<Int>()
-        for _ in 0..<10000 {
-            let random = Int.random(lowerBound: -50, upperBound: 50)
-            XCTAssert(random >= -50 && random < 50)
-            values.insert(random)
-        }
-        XCTAssertEqual(values.count, 100)
-    }
-
-    func testRandomUInts() {
-        var values = Set<UInt>()
-        for _ in 0..<10000 {
-            let random = UInt.random(upperBound: 100)
-            XCTAssert(random >= 0 && random < 100)
-            values.insert(random)
-        }
-        XCTAssertEqual(values.count, 100)
-    }
-
-    func testMinInt64s() {
-        var values = Set<Int64>()
-        for _ in 0..<10000 {
-            let min = Int64.min
-            let max = min + 100
-            let random = Int64.random(lowerBound: min, upperBound: max)
-            XCTAssert(random >= min && random < max)
-            values.insert(random)
-        }
-        XCTAssertEqual(values.count, 100)
-    }
-
-    func testMaxInt64s() {
-        var values = Set<Int64>()
-        for _ in 0..<10000 {
-            let max = Int64.max
-            let min = max - 100
-            let random = Int64.random(lowerBound: min, upperBound: max)
-            XCTAssert(random >= min && random < max)
-            values.insert(random)
-        }
-        XCTAssertEqual(values.count, 100)
-    }
-
-    func testMaxUInt32s() {
-        var values = Set<UInt32>()
-        for _ in 0..<10000 {
-            let max = UInt32.max
-            let min = max - 100
-            let random = UInt32.random(lowerBound: min, upperBound: max)
-            XCTAssert(random >= min && random < max)
-            values.insert(random)
-        }
-        XCTAssertEqual(values.count, 100)
+        runBinaryIntegers(lowerBound: -50, upperBound: 50)
     }
 
 }
