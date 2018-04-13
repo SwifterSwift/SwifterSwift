34898db538c50481b2e982bec2ba8f51a09ce66a
Guy Kogus <guy.kogus@gmail.com>
2018-04-13 15:22:49 +0200

    Add some comments and assertions for generating random floating point numbers to warn developers of potential pitfalls.

--- Sources/Extensions/SwiftStdlib/BinaryFloatingPointExtensions.swift
+++ Sources/Extensions/SwiftStdlib/BinaryFloatingPointExtensions.swift
@@ -7,13 +7,19 @@
 //
 
 public extension BinaryFloatingPoint {
+
     /// SwifterSwift: Generate a random floating point number within a given range.
     ///
     /// - Parameters:
     ///   - lowerBound: The lower bound of the floating point range. Defaults to 0.0.
     ///   - upperBound: The upper bound of the floating point range. Defaults to 1.0.
     /// - Returns: A floating point between `lowerBound` and `upperBound` (inclusive).
+    /// - Warning: If `upperBound - lowerBound` has a non-normal value (e.g. `infinity` or `nan`) the result is undefined.
     public static func random(lowerBound: Self = 0.0, upperBound: Self = 1.0) -> Self {
-        return lowerBound + (Self(drand48()) * (upperBound - lowerBound))
+        assert(lowerBound <= upperBound)
+        let distance = upperBound - lowerBound
+        assert(distance.isNormal)
+        return lowerBound + (Self(drand48()) * distance)
     }
+
 }
--- Tests/SwiftStdlibTests/BinaryFloatingPointExtensionsTests.swift
+++ Tests/SwiftStdlibTests/BinaryFloatingPointExtensionsTests.swift
@@ -11,6 +11,8 @@ import XCTest
 
 final class BinaryFloatingPointExtensionsTests: XCTestCase {
 
+    let testRuns = 10000
+
     func testRandomDoubles() {
         var values = Set<Double>()
         for _ in 0..<10000 {
@@ -29,6 +31,30 @@ final class BinaryFloatingPointExtensionsTests: XCTestCase {
             values.insert(random)
         }
         XCTAssertEqual(values.count, 10000)
+    }
+
+    func testLargePositiveDoubles() {
+        var values = Set<Double>(minimumCapacity: testRuns)
+        let lowerBound = 0.0
+        let upperBound = Double.greatestFiniteMagnitude
+        for _ in 0..<testRuns {
+            let random = Double.random(lowerBound: lowerBound, upperBound: upperBound)
+            XCTAssert(random >= lowerBound && random < upperBound)
+            values.insert(random)
+        }
+        XCTAssertEqual(values.count, testRuns)
+    }
+
+    func testLargeNegativeDoubles() {
+        var values = Set<Double>(minimumCapacity: testRuns)
+        let lowerBound = -Double.greatestFiniteMagnitude
+        let upperBound = 0.0
+        for _ in 0..<testRuns {
+            let random = Double.random(lowerBound: lowerBound, upperBound: upperBound)
+            XCTAssert(random >= lowerBound && random < upperBound)
+            values.insert(random)
+        }
+        XCTAssertEqual(values.count, testRuns)
     }
 
 }
