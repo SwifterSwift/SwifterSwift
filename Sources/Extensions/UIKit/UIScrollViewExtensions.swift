60c435c0d03a2db9a7d526540514bcf668d1d0ba
Camila Resende <aliamcami@gmail.com>
2018-04-22 19:21:47 -0300

    Remove unnecessary whitespaces (bot request)

--- Sources/Extensions/UIKit/UIScrollViewExtensions.swift
+++ Sources/Extensions/UIKit/UIScrollViewExtensions.swift
@@ -8,13 +8,10 @@
 
 #if canImport(UIKit)
     import UIKit
-    
 #if !os(watchOS)
 
 // MARK: - Methods
 public extension UIScrollView{
-    
-    
     //Original Source: https://gist.github.com/thestoics/1204051
     /// SwifterSwift: Takes a snapshot of an entire ScrollView
     ///
@@ -22,22 +19,21 @@ public extension UIScrollView{
     ///    UITableView().snapshot
     ///
     /// - Returns: Snapshot as UIimage for rendered ScrollView
-    public var snapshot: UIImage? {
+    public var snapshot: UIImage?{
         UIGraphicsBeginImageContextWithOptions(contentSize, false, 0)
-        defer {
+        defer{
             UIGraphicsEndImageContext()
         }
         guard let context = UIGraphicsGetCurrentContext() else { return nil }
-        
         let previousFrame = frame
         frame = CGRect(origin: frame.origin, size: contentSize)
         layer.render(in: context)
         frame = previousFrame
-        
         layer.render(in: context)
         return UIGraphicsGetImageFromCurrentImageContext()
     }
 }
+
 #endif
-    
+
 #endif
--- Tests/UIKitTests/UIScrollViewExtensionsTest.swift
+++ Tests/UIKitTests/UIScrollViewExtensionsTest.swift
@@ -12,13 +12,11 @@ import XCTest
 #if os(iOS) || os(tvOS)
 final class UIScrollViewExtensionsTest: XCTestCase {
     func testSnapshot() {
-        
         let frame = CGRect(x: 0, y: 0, width: 100, height: 100)
         let scroll = UIScrollView(frame: frame)
         scroll.contentSize = frame.size
         let snapshot = scroll.snapshot
         XCTAssertNotNil(snapshot)
-        
         let view = UIScrollView()
         XCTAssertNil(view.snapshot)
     } 
