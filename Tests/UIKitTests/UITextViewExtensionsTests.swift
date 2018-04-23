9d2c5654c63a455e282af93206ff1a3f344a5a3a
Ratul sharker <sharker.ratul.08@gmail.com>
2018-04-23 11:13:28 +0600

    one empty line spacing removed for swift lint.

--- Tests/UIKitTests/UITextViewExtensionsTests.swift
+++ Tests/UIKitTests/UITextViewExtensionsTests.swift
@@ -42,7 +42,7 @@ final class UITextViewExtensionsTests: XCTestCase {
 
     func testWrapToContent() {
         let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
- 
+
         // initial setting
         textView.frame = CGRect.init(x: 0, y: 0, width: 100, height: 20)
         textView.font = UIFont.systemFont(ofSize: 20.0)
