fcdfbc505315b1b54c395f9ae8399a6e534a3c74
Ratul sharker <sharker.ratul.08@gmail.com>
2018-04-27 01:29:48 +0600

    unnecessary `init` is removed from the test case.

--- Tests/UIKitTests/UITextViewExtensionsTests.swift
+++ Tests/UIKitTests/UITextViewExtensionsTests.swift
@@ -44,7 +44,7 @@ final class UITextViewExtensionsTests: XCTestCase {
         let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
         // initial setting
-        textView.frame = CGRect.init(x: 0, y: 0, width: 100, height: 20)
+        textView.frame = CGRect(x: 0, y: 0, width: 100, height: 20)
         textView.font = UIFont.systemFont(ofSize: 20.0)
         textView.text = text
 
@@ -55,7 +55,7 @@ final class UITextViewExtensionsTests: XCTestCase {
                                             attributes: [.font: textView.font!],
                                             context: nil)
         let textHeight = ceil(boundingBox.height)
-        let textSize = CGSize.init(width: 100, height: textHeight)
+        let textSize = CGSize(width: 100, height: textHeight)
 
         // before setting wrap, content won't be equal to bounds
         XCTAssertNotEqual(textView.bounds.size, textView.contentSize)
@@ -68,7 +68,7 @@ final class UITextViewExtensionsTests: XCTestCase {
         // This is important to set the frame after calling the wrapToContent, otherwise
         // boundingRect can give you fractional value, and method call `sizeToFit` inside the
         // wrapToContent would change to the fractional value instead of the ceil value.
-        textView.bounds = CGRect.init(x: 0, y: 0, width: textSize.width, height: textSize.height)
+        textView.bounds = CGRect(x: 0, y: 0, width: textSize.width, height: textSize.height)
 
         // after setting wrap, content size will be equal to bounds
         XCTAssertEqual(textView.bounds.size, textView.contentSize)
