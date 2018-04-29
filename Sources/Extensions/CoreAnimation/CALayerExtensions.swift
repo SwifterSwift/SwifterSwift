44ceb209419a9a5167a05e0762ed4942e0f60af8
Morgan Dock <morgan.dock@gmail.com>
2018-04-29 14:14:49 -0700

    update extension to be excluded from watchOS, make color non-default to support macOS

--- Sources/Extensions/CoreAnimation/CALayerExtensions.swift
+++ Sources/Extensions/CoreAnimation/CALayerExtensions.swift
@@ -6,19 +6,20 @@
 //  Copyright © 2018 SwifterSwift
 //
 
+#if !os(watchOS)
+
 public extension CALayer {
 
     /// SwifterSwift: Create a shadow affect, using the same parameters used in design software such as Sketch, Zeplin, and others.
     /// This Extension originates from here: https://stackoverflow.com/questions/34269399/how-to-control-shadow-spread-and-blur/34270362
     ///
     /// - Parameters:
-    ///   - color: the tint of the shadow effect.  Default is UIColor.black
-    ///   - alpha: opacity of the color applied to the shaddow effect.  Default is 0.5
-    ///   - x: the horizontal offset applied to the shadow effect.  Default is 0.
-    ///   - y: the vertical offset applied to the shadow effect.  Default is 0.
-    ///   - blur: if set to 0 the shadow will be sharp, the higher the number, the more blurred it will be.  Default is 0.
+    ///   - color: the tint of the shadow effect.  No Default is provided
+    ///   - alpha: opacity of the color applied to the shaddow effect.  Default is 0.08
+    ///   - offset: the x and y offsets represented as a CGSize.  Default is (0, 3)
+    ///   - blur: if set to 0 the shadow will be sharp, the higher the number, the more blurred it will be.  Default is 5.
     ///   - spread: positive values increase the size of the shadow, negative values decrease the size. Default is 0.
-    public func applyShadow(color: CGColor = UIColor.black.cgColor, alpha: Float = 0.5, offset: CGSize = CGSize(width: 0, height: 0), blur: CGFloat = 0, spread: CGFloat = 0) {
+    public func applyShadow(withColor color: CGColor, alpha: Float = 0.08, offset: CGSize = CGSize(width: 0, height: 3), blur: CGFloat = 5, spread: CGFloat = 0) {
         shadowColor = color
         shadowOpacity = alpha
         shadowOffset = offset
@@ -30,4 +31,6 @@ public extension CALayer {
             shadowPath = CGPath(rect: bounds.insetBy(dx: dirX, dy: dirX), transform: nil)
         }
     }
+
 }
+#endif
