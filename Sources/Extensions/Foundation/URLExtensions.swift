c51b8433e28f140c956d9bd7be6504404dd4e661
Ratul Sharker <ratul.sharker@ipay.com.bd>
2018-05-03 21:39:48 +0600

    New init method added in URL class to initialize an URL applying percent encoding.

--- Sources/Extensions/Foundation/URLExtensions.swift
+++ Sources/Extensions/Foundation/URLExtensions.swift
@@ -29,12 +29,26 @@ public extension URL {
 
 		return items
 	}
-
 }
 
 // MARK: - Methods
 public extension URL {
 
+    /// SwifterSwift: URL initiate with percent encoding
+    ///
+    ///     let urlString = "http://www.prothomalo.com/bangladesh/article/1472031/নারায়ণগঞ্জে-গ্যাসবোমার-ওপর-বসবাস-তাঁদের"
+    ///     let url = URL.initWithPercentEncoding(urlString)
+    ///     url -> "http://www.prothomalo.com/bangladesh/article/1472031/%E0%A6%A8%E0%A6%BE%E0%A6%B0%E0%A6%BE%E0%A6%AF%E0%A6%BC%E0%A6%A3%E0%A6%97%E0%A6%9E%E0%A7%8D%E0%A6%9C%E0%A7%87-%E0%A6%97%E0%A7%8D%E0%A6%AF%E0%A6%BE%E0%A6%B8%E0%A6%AC%E0%A7%8B%E0%A6%AE%E0%A6%BE%E0%A6%B0-%E0%A6%93%E0%A6%AA%E0%A6%B0-%E0%A6%AC%E0%A6%B8%E0%A6%AC%E0%A6%BE%E0%A6%B8-%E0%A6%A4%E0%A6%BE%E0%A6%81%E0%A6%A6%E0%A7%87%E0%A6%B0"
+    ///
+    /// - Parameters: string representation of an url
+    /// - Returns: initated url using the percent encoding
+    public init?(withPercentEncodingOfString string: String) {
+        if let encodedString = string.addingPercentEncoding(withAllowedCharacters: .urlFragmentAllowed) {
+            self.init(string: encodedString)
+        } else {
+            return nil
+        }
+    }
 	/// SwifterSwift: URL with appending query parameters.
 	///
 	///		let url = URL(string: "https://google.com")!
--- Tests/FoundationTests/URLExtensionsTests.swift
+++ Tests/FoundationTests/URLExtensionsTests.swift
@@ -15,6 +15,16 @@ final class URLExtensionsTests: XCTestCase {
 	let params = ["q": "swifter swift"]
 	let queryUrl = URL(string: "https://www.google.com?q=swifter%20swift")!
 
+    func testInitWithPercentEncoding() {
+        let urlWithUnicode = "http://www.prothomalo.com/bangladesh/article/1472031/নারায়ণগঞ্জে-গ্যাসবোমার-ওপর-বসবাস-তাঁদের"
+        let percentEncodedURL = "http://www.prothomalo.com/bangladesh/article/1472031/%E0%A6%A8%E0%A6%BE%E0%A6%B0%E0%A6%BE%E0%A6%AF%E0%A6%BC%E0%A6%A3%E0%A6%97%E0%A6%9E%E0%A7%8D%E0%A6%9C%E0%A7%87-%E0%A6%97%E0%A7%8D%E0%A6%AF%E0%A6%BE%E0%A6%B8%E0%A6%AC%E0%A7%8B%E0%A6%AE%E0%A6%BE%E0%A6%B0-%E0%A6%93%E0%A6%AA%E0%A6%B0-%E0%A6%AC%E0%A6%B8%E0%A6%AC%E0%A6%BE%E0%A6%B8-%E0%A6%A4%E0%A6%BE%E0%A6%81%E0%A6%A6%E0%A7%87%E0%A6%B0"
+
+        XCTAssertNil(URL.init(string: urlWithUnicode))
+        XCTAssertNotNil(URL.init(withPercentEncodingOfString: urlWithUnicode))
+        XCTAssertEqual(URL.init(withPercentEncodingOfString: urlWithUnicode)?.absoluteString, percentEncodedURL)
+
+    }
+
 	func testAppendingQueryParameters() {
 		XCTAssertEqual(url.appendingQueryParameters(params), queryUrl)
 	}
