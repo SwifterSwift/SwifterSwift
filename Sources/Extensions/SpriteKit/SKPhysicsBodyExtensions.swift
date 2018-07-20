2566e69a2283435cdb04e3f9ea59845f7dd9858a
Robbie Moyer <rmoyer08@gmail.com>
2018-07-20 10:30:05 -0400

    SKPhysicsBody extension

---
+++ Sources/Extensions/SpriteKit/SKPhysicsBodyExtensions.swift
@@ -0,0 +1,23 @@
+//
+//  SKPhysicsBodyExtensions.swift
+//  SwifterSwift
+//
+//  Created by Robbie Moyer on 7/20/18.
+//  Copyright © 2018 SwifterSwift
+//
+
+import SpriteKit
+
+public extension SKPhysicsBody {
+    
+    
+    /// SwifterSwift: Checks if a physics body is contacting another body
+    /// with the given `categoryBitMask`
+    ///
+    /// - Parameter category: The `categoryBitmMask` to check
+    /// - Returns: `true` if the physics body is contacting another body
+    ///             with the given bitmask, `false` otherwise.
+    public func isTouching(_ category: UInt32) -> Bool {
+        return allContactedBodies().contains(where: { $0.categoryBitMask == category })
+    }
+}
