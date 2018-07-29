706487adabe3f597ba8ccb1903af30fe123918fa
Marco Capano <marco24capano@icloud.com>
2018-07-29 17:22:12 +0200

    Update UIViewControllerExtensions.swift

--- Sources/Extensions/UIKit/UIViewControllerExtensions.swift
+++ Sources/Extensions/UIKit/UIViewControllerExtensions.swift
@@ -82,14 +82,14 @@ public extension UIViewController {
     ///
     /// - Parameters:
     ///   - child: the view controller to add as a child
-    func add(_ child: UIViewController) {
+    public func add(_ child: UIViewController) {
         addChildViewController(child)
         view.addSubview(child.view)
         child.didMove(toParentViewController: self)
     }
     
     /// SwifterSwift: Helper method to remove a UIViewController from its parent.
-    func remove() {
+    public func remove() {
         guard parent != nil else {
             return
         }
