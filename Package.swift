e9742d955dd70dbf3f82cefb18fdc80b91b24e27
Ratul Sharker <ratul.sharker@ipay.com.bd>
2018-08-25 04:15:39 +0600

    Swiftlint correction is done.

--- Package.swift
+++ Package.swift
@@ -9,17 +9,17 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwifterSwift",
-            targets: ["Extensions"]),
+            targets: ["Extensions"])
         ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.SwiftStdlib
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Extensions",
-            dependencies: []),
+            dependencies: [])
         ]
 )
