ca295b4a271f637bd423a469f2568ee3d5dda6d9
Luciano Almeida <passos.luciano@outlook.com>
2018-09-14 23:34:52 -0300

    Indenting.

--- Tests/UIKitTests/UIViewControllerExtensionsTests.swift
+++ Tests/UIKitTests/UIViewControllerExtensionsTests.swift
@@ -14,111 +14,111 @@ import UIKit
 
 final class UIViewControllerExtensionsTests: XCTestCase {
 
-    class MockNotificationViewController: UIViewController {
-        var notificationFired = false
+        class MockNotificationViewController: UIViewController {
+                var notificationFired = false
 
-        @objc func testSelector() {
-            notificationFired = true
+                @objc func testSelector() {
+                        notificationFired = true
+                }
         }
-    }
 
-    let notificationIdentifier = Notification.Name("MockNotification")
+        let notificationIdentifier = Notification.Name("MockNotification")
 
-    func testAddNotificationObserver() {
-        let viewController = MockNotificationViewController()
-        let selector = #selector(MockNotificationViewController.testSelector)
-        viewController.addNotificationObserver(name: notificationIdentifier, selector: selector)
-        NotificationCenter.default.post(name: notificationIdentifier, object: nil)
-        XCTAssert(viewController.notificationFired)
-    }
-
-    func testIsVisible() {
-        let viewController = UIViewController()
-        XCTAssertFalse(viewController.isVisible)
-    }
-
-    func testRemoveNotificationObserver() {
-        let viewController = MockNotificationViewController()
-        let selector = #selector(MockNotificationViewController.testSelector)
-        viewController.addNotificationObserver(name: notificationIdentifier, selector: selector)
-        viewController.removeNotificationObserver(name: notificationIdentifier)
-        NotificationCenter.default.post(name: notificationIdentifier, object: nil)
-        XCTAssertFalse(viewController.notificationFired)
-    }
-
-    func testRemoveNotificationsObserver() {
-        let viewController = MockNotificationViewController()
-        let selector = #selector(MockNotificationViewController.testSelector)
-        viewController.addNotificationObserver(name: notificationIdentifier, selector: selector)
-        viewController.removeNotificationsObserver()
-        NotificationCenter.default.post(name: notificationIdentifier, object: nil)
-        XCTAssertFalse(viewController.notificationFired)
-    }
-
-    func testShowAlert() {
-        let viewController = UIViewController()
-        UIApplication.shared.keyWindow?.rootViewController = viewController
-        let title = "test title"
-        let message = "test message"
-        let actionButtons = ["OK", "Cancel"]
-        let preferredButtonIndex = 1
-        let alertController = viewController.showAlert(
-            title: title,
-            message: message,
-            buttonTitles: actionButtons,
-            highlightedButtonIndex: preferredButtonIndex,
-            completion: nil)
-
-        XCTAssertEqual(alertController.preferredStyle, .alert)
-        XCTAssertEqual(alertController.title, title)
-        XCTAssertEqual(alertController.message, message)
-        //check whether the buttons are added in the same order
-        for action in 0..<alertController.actions.count {
-            XCTAssertEqual(alertController.actions[action].title, actionButtons[action])
+        func testAddNotificationObserver() {
+                let viewController = MockNotificationViewController()
+                let selector = #selector(MockNotificationViewController.testSelector)
+                viewController.addNotificationObserver(name: notificationIdentifier, selector: selector)
+                NotificationCenter.default.post(name: notificationIdentifier, object: nil)
+                XCTAssert(viewController.notificationFired)
         }
-        XCTAssertEqual(alertController.preferredAction, alertController.actions[preferredButtonIndex])
-    }
 
-    func testAddChildViewController() {
-        let parentViewController = UIViewController()
-        let childViewController = UIViewController()
+        func testIsVisible() {
+                let viewController = UIViewController()
+                XCTAssertFalse(viewController.isVisible)
+        }
 
-        XCTAssert(parentViewController.children.isEmpty)
-        XCTAssertNil(childViewController.parent)
+        func testRemoveNotificationObserver() {
+                let viewController = MockNotificationViewController()
+                let selector = #selector(MockNotificationViewController.testSelector)
+                viewController.addNotificationObserver(name: notificationIdentifier, selector: selector)
+                viewController.removeNotificationObserver(name: notificationIdentifier)
+                NotificationCenter.default.post(name: notificationIdentifier, object: nil)
+                XCTAssertFalse(viewController.notificationFired)
+        }
 
-        parentViewController.addChildViewController(childViewController, toContainerView: parentViewController.view)
+        func testRemoveNotificationsObserver() {
+                let viewController = MockNotificationViewController()
+                let selector = #selector(MockNotificationViewController.testSelector)
+                viewController.addNotificationObserver(name: notificationIdentifier, selector: selector)
+                viewController.removeNotificationsObserver()
+                NotificationCenter.default.post(name: notificationIdentifier, object: nil)
+                XCTAssertFalse(viewController.notificationFired)
+        }
 
-        XCTAssertEqual(parentViewController.children, [childViewController])
-        XCTAssertNotNil(childViewController.parent)
-        XCTAssertEqual(childViewController.parent, parentViewController)
-    }
+        func testShowAlert() {
+                let viewController = UIViewController()
+                UIApplication.shared.keyWindow?.rootViewController = viewController
+                let title = "test title"
+                let message = "test message"
+                let actionButtons = ["OK", "Cancel"]
+                let preferredButtonIndex = 1
+                let alertController = viewController.showAlert(
+                        title: title,
+                        message: message,
+                        buttonTitles: actionButtons,
+                        highlightedButtonIndex: preferredButtonIndex,
+                        completion: nil)
 
-    func testRemoveChildViewController() {
-        let parentViewController = UIViewController()
-        let childViewController = UIViewController()
+                XCTAssertEqual(alertController.preferredStyle, .alert)
+                XCTAssertEqual(alertController.title, title)
+                XCTAssertEqual(alertController.message, message)
+                //check whether the buttons are added in the same order
+                for action in 0..<alertController.actions.count {
+                        XCTAssertEqual(alertController.actions[action].title, actionButtons[action])
+                }
+                XCTAssertEqual(alertController.preferredAction, alertController.actions[preferredButtonIndex])
+        }
 
-        parentViewController.addChild(childViewController)
-        parentViewController.view.addSubview(childViewController.view)
-        childViewController.didMove(toParent: parentViewController)
+        func testAddChildViewController() {
+                let parentViewController = UIViewController()
+                let childViewController = UIViewController()
 
-        XCTAssertEqual(parentViewController.children, [childViewController])
-        XCTAssertNotNil(childViewController.parent)
-        XCTAssertEqual(childViewController.parent, parentViewController)
+                XCTAssert(parentViewController.children.isEmpty)
+                XCTAssertNil(childViewController.parent)
 
-        childViewController.removeViewAndControllerFromParentViewController()
+                parentViewController.addChildViewController(childViewController, toContainerView: parentViewController.view)
 
-        XCTAssert(parentViewController.children.isEmpty)
-        XCTAssertNil(childViewController.parent)
-    }
+                XCTAssertEqual(parentViewController.children, [childViewController])
+                XCTAssertNotNil(childViewController.parent)
+                XCTAssertEqual(childViewController.parent, parentViewController)
+        }
 
-    func testRemoveChildViewControllerWithNoParent() {
-        let childViewController = UIViewController()
-        XCTAssertNil(childViewController.parent)
+        func testRemoveChildViewController() {
+                let parentViewController = UIViewController()
+                let childViewController = UIViewController()
 
-        childViewController.removeViewAndControllerFromParentViewController()
+                parentViewController.addChild(childViewController)
+                parentViewController.view.addSubview(childViewController.view)
+                childViewController.didMove(toParent: parentViewController)
 
-        XCTAssertNil(childViewController.parent)
-    }
+                XCTAssertEqual(parentViewController.children, [childViewController])
+                XCTAssertNotNil(childViewController.parent)
+                XCTAssertEqual(childViewController.parent, parentViewController)
+
+                childViewController.removeViewAndControllerFromParentViewController()
+
+                XCTAssert(parentViewController.children.isEmpty)
+                XCTAssertNil(childViewController.parent)
+        }
+
+        func testRemoveChildViewControllerWithNoParent() {
+                let childViewController = UIViewController()
+                XCTAssertNil(childViewController.parent)
+
+                childViewController.removeViewAndControllerFromParentViewController()
+
+                XCTAssertNil(childViewController.parent)
+        }
 
 }
 #endif
