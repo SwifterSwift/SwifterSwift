9a703fdaed22f67afab60bc691c940901032cf2d
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-29 15:14:53 -0500

    Use .isEmptry instead of .count check for path length guard in Dictionary[path:]

--- Sources/Extensions/SwiftStdlib/DictionaryExtensions.swift
+++ Sources/Extensions/SwiftStdlib/DictionaryExtensions.swift
@@ -165,7 +165,7 @@ public extension Dictionary {
     /// - Returns: The value for the key-path passed in. `nil` if no value is found.
     public subscript(path path: [Key]) -> Any? {
         get {
-            guard path.count > 0 else { return nil }
+            guard !path.isEmpty else { return nil }
             var result: Any? = self
             for key in path {
                 if let element = (result as? [Key: Any])?[key] {
