12e88a5927c29e0d0de2b3f6f8c010e974d6ff03
QuentinJin <jianstm@gmail.com>
2018-10-13 22:02:51 +0800

    Make method public #583

--- Sources/Extensions/SwiftStdlib/BinaryFloatingPointExtensions.swift
+++ Sources/Extensions/SwiftStdlib/BinaryFloatingPointExtensions.swift
@@ -7,7 +7,7 @@
 //
 
 // MARK: - Methods
-extension BinaryFloatingPoint {
+public extension BinaryFloatingPoint {
 
     /// SwifterSwift: Returns a rounded value with the specified number of
     /// decimal places and rounding rule. If `numberOfDecimalPlaces` is negative,
@@ -24,7 +24,7 @@ extension BinaryFloatingPoint {
     ///   - numberOfDecimalPlaces: The expected number of decimal places.
     ///   - rule: The rounding rule to use.
     /// - Returns: The rounded value.
-    func rounded(numberOfDecimalPlaces: Int, rule: FloatingPointRoundingRule) -> Self {
+    public func rounded(numberOfDecimalPlaces: Int, rule: FloatingPointRoundingRule) -> Self {
         let factor = Self(pow(10.0, Double(max(0, numberOfDecimalPlaces))))
         return (self * factor).rounded(rule) / factor
     }
