3fb448353208ba50005283d9a83c94c408674cee
Daniel Lin <linzhdk@gmail.com>
2018-10-16 17:04:20 +0800

    Add an project of installation example with Cocoapods

---
+++ Examples/PodExample-iOS/PodExample-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PodExample-iOS
+//
+//  Created by Daniel Lin on 2018/10/16.
+//  Copyright © 2018 SwifterSwift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
