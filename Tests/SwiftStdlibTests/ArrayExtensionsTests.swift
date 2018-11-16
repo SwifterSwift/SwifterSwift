770243923d8cf83f7a1a60fb8ecae00566d42470
Difeng Chen <dylancfe15@gmail.com>
2018-11-15 21:19:29 -0500

    Deleted testCountElement()

--- Tests/SwiftStdlibTests/ArrayExtensionsTests.swift
+++ Tests/SwiftStdlibTests/ArrayExtensionsTests.swift
@@ -148,11 +148,6 @@ final class ArrayExtensionsTests: XCTestCase {
         XCTAssertEqual([1, 1, 2, 2, 3, 3, 3, 4, 5].withoutDuplicates(), [1, 2, 3, 4, 5])
         XCTAssertEqual(["h", "e", "l", "l", "o"].withoutDuplicates(), ["h", "e", "l", "o"])
     }
-
-    func testCountElement(){
-        XCTAssertEqual([1, 1, 2, 2, 3, 3, 3, 4, 5].countElement(),[1:2,2:2,3:3,4:1,5:1])
-        XCTAssertEqual([1, 1, 2, 2, 3, 3, 3, 4, 5].countElement(2),2)
-    }
     
     func testGetDuplicates(){
         XCTAssertEqual([1, 1, 2, 2, 3, 3, 3, 4, 5].getDuplicates(), [1, 2, 3])
