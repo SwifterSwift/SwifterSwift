0b56b15e8c4ed6726c09e336445f6336817615a9
towry <tovvry@gmail.com>
2018-12-24 08:52:58 +0800

    Fix typo

--- Sources/Extensions/CoreGraphics/Deprecated/CoreGraphicsDeprecated.swift
+++ Sources/Extensions/CoreGraphics/Deprecated/CoreGraphicsDeprecated.swift
@@ -17,7 +17,7 @@ public extension CGFloat {
     ///   - min: minimum number to start random from.
     ///   - max: maximum number random number end before.
     /// - Returns: random CGFloat between two CGFloat values.
-    @available(*, deprecated: 4.5.0, message: "User CGFloat.random(in: min...max)")
+    @available(*, deprecated: 4.5.0, message: "Use CGFloat.random(in: min...max)")
     public static func randomBetween(min: CGFloat, max: CGFloat) -> CGFloat {
         return CGFloat.random(in: min...max)
     }
