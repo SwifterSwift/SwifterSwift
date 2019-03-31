8f0bff2fd14e1fa4228bd93832cb533848cfee61
Omar Albeik <omaralbeik@gmail.com>
2019-03-31 15:39:47 +0300

    Attempt Package.swift fix

--- Package.swift
+++ Package.swift
@@ -6,18 +6,12 @@ import PackageDescription
 let package = Package(
     name: "SwifterSwift",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwifterSwift",
-            targets: ["SwifterSwift"])
+            targets: ["Extensions"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.SwiftStdlib
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwifterSwift",
             dependencies: [])
