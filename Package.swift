ddcfa6ed54d12ea89260036a5a2a34bf6755278f
Omar Albeik <omaralbeik@gmail.com>
2019-03-31 15:37:10 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwifterSwift",
-            targets: ["Extensions"])
+            targets: ["SwifterSwift"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.SwiftStdlib
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "Extensions",
+            name: "SwifterSwift",
             dependencies: [])
     ]
 )
