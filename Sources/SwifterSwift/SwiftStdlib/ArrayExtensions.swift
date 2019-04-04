ba870c8a274554872ca901306778ea878bfe55fa
calm <calm@nb.com>
2019-04-04 19:40:06 +0800

    Add Array Extension for filtered

--- Sources/SwifterSwift/SwiftStdlib/ArrayExtensions.swift
+++ Sources/SwifterSwift/SwiftStdlib/ArrayExtensions.swift
@@ -161,5 +161,34 @@ public extension Array where Element: Equatable {
             }
         }
     }
-
 }
+
+extension Array where Element: Hashable {
+    
+    public func filtered<E: Equatable>(duplication path: KeyPath<Element, E>) -> [Element] {
+        return reduce(into: [Element]()) { (result, e) in
+            let contains = result.contains { $0[keyPath: path] == e[keyPath: path] }
+            result += contains ? [] : [e]
+        }
+    }
+    
+    public func filtered<E: Equatable>(duplication closure: (Element) throws -> E) rethrows -> [Element] {
+        return try reduce(into: [Element]()) { (result, e) in
+            let contains = try result.contains { try closure($0) == closure(e) }
+            result += contains ? [] : [e]
+        }
+    }
+    
+    @discardableResult
+    public mutating func filter<E: Equatable>(duplication path: KeyPath<Element, E>) -> [Element] {
+        self = filtered(duplication: path)
+        return self
+    }
+    
+    @discardableResult
+    public mutating func filter<E: Equatable>(duplication closure: (Element) throws -> E) rethrows -> [Element] {
+        self = try filtered(duplication: closure)
+        return self
+    }
+}
+
