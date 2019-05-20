9d6c2f0355a726750488714b3d5db7548138f062
Luciano Almeida <passos.luciano@outlook.com>
2019-05-20 10:42:57 -0300

    Fixing missing public modifiers

--- Sources/SwifterSwift/UIKit/UILayoutPriorityExtensions.swift
+++ Sources/SwifterSwift/UIKit/UILayoutPriorityExtensions.swift
@@ -9,7 +9,7 @@
 #if os(iOS) || os(tvOS)
 import UIKit
 
-extension UILayoutPriority: ExpressibleByFloatLiteral, ExpressibleByIntegerLiteral {
+public extension UILayoutPriority: ExpressibleByFloatLiteral, ExpressibleByIntegerLiteral {
 
     // MARK: - Initializers
 
@@ -18,7 +18,7 @@ extension UILayoutPriority: ExpressibleByFloatLiteral, ExpressibleByIntegerLiter
     ///     constraint.priority = 0.5
     ///
     /// - Parameter value: The float value of the constraint
-    public init(floatLiteral value: Float) {
+    init(floatLiteral value: Float) {
         self.init(rawValue: value)
     }
 
@@ -27,7 +27,7 @@ extension UILayoutPriority: ExpressibleByFloatLiteral, ExpressibleByIntegerLiter
     ///     constraint.priority = 5
     ///
     /// - Parameter value: The integer value of the constraint
-    public init(integerLiteral value: Int) {
+    init(integerLiteral value: Int) {
         self.init(rawValue: Float(value))
     }
 
