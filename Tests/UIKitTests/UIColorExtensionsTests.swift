d9e4dba9e864edaee44f9ec3187d2fca0696d6bf
Max Härtwig <vyaxxx@gmail.com>
2019-10-06 11:26:20 +0200

    Add init(light:dark:) to NSColor and UIColor

---
+++ Tests/UIKitTests/UIColorExtensionsTests.swift
@@ -0,0 +1,23 @@
+//
+//  UIColorExtensionsTests.swift
+//  SwifterSwift
+//
+//  Created by Max Haertwig on 10/06/19.
+//  Copyright © 2019 SwifterSwift
+//
+
+import XCTest
+@testable import SwifterSwift
+
+final class UIColorExtensionsTests: XCTestCase {
+
+    @available(iOS 13.0, tvOS 13.0, *)
+    func testInitLightDark() {
+        let lightModeColor = UIColor.red
+        let darkModeColor = UIColor.blue
+        let color = UIColor(light: lightModeColor, dark: darkModeColor)
+        XCTAssertEqual(color.resolvedColor(with: UITraitCollection(userInterfaceStyle: .light)), lightModeColor)
+        XCTAssertEqual(color.resolvedColor(with: UITraitCollection(userInterfaceStyle: .dark)), darkModeColor)
+    }
+
+}
