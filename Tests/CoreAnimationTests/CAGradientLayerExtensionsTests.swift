e94273d52e737ce0a20c2b9c94175f5a2514a9c2
Jay Mehta <jaymmehta97@gmail.com>
2019-10-15 14:30:25 +0530

    Added non-nil values to locations for test.

--- Tests/CoreAnimationTests/CAGradientLayerExtensionsTests.swift
+++ Tests/CoreAnimationTests/CAGradientLayerExtensionsTests.swift
@@ -15,10 +15,10 @@ final class CAGradientLayerExtensionsTests: XCTestCase {
 
     func testInitWithGradientAttributes() {
         let colors: [UIColor] = [.red, .blue, .orange, .yellow]
-        let locations: [CGFloat]? = nil
+        let locations: [CGFloat]? = [0, 0.3, 0.6, 1]
         let startPoint = CGPoint(x: 0.0, y: 0.5)
         let endPoint = CGPoint(x: 1.0, y: 0.5)
-        let gradientLayerType: CAGradientLayerType = .axial
+        let gradientLayerType = CAGradientLayerType.axial
 
         let gradientLayer = CAGradientLayer(colors: colors, locations: locations, startPoint: startPoint, endPoint: endPoint, type: gradientLayerType)
 
