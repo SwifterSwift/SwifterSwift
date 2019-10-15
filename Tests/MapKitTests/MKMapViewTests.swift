174aae9cfe143ec703cc3433734c9b904f12ecbb
Mustafa GÜNEŞ <gunes149@gmail.com>
2019-10-15 16:13:09 +0300

    Update Tests/MapKitTests/MKMapViewTests.swift
    Co-Authored-By: Guy Kogus <guy.kogus@gmail.com>

--- Tests/MapKitTests/MKMapViewTests.swift
+++ Tests/MapKitTests/MKMapViewTests.swift
@@ -51,7 +51,7 @@ final class MKMapViewTests: XCTestCase {
         let mapView = MKMapView()
         
         let meter = 500.0
-        let oneItemArray: [CLLocationCoordinate2D] = [.init(latitude: 36.9751, longitude: 38.4243)]
+        let oneItemArray = [CLLocationCoordinate2D(latitude: 36.9751, longitude: 38.4243)]
         let edgePadding: UIEdgeInsets = .init(top: 50, left: 50, bottom: 50, right: 50)
         let oneItemInMapView: Void = mapView.zoom(to: oneItemArray, meter: meter, edgePadding: edgePadding, animated: true)
         
