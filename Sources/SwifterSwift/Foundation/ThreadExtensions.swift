ae5e9ad6313c161d182a024fe3bbfd8577cdaccb
Francesco Deliro <f.deliro@gmail.com>
2019-10-18 22:41:53 +0200

    Add Thread Extensions and tests

---
+++ Sources/SwifterSwift/Foundation/ThreadExtensions.swift
@@ -0,0 +1,34 @@
+//
+//  ThreadExtensions.swift
+//  SwifterSwift
+//
+//  Created by Francesco Deliro on 18/10/2019.
+//  Copyright © 2019 SwifterSwift
+//
+
+#if canImport(Foundation)
+import Foundation
+
+// MARK: - Methods
+public extension Thread {
+
+    /// SwifterSwift: Execute a completion block on the main thread.
+    ///
+    /// - Parameter completion: The completion to execute.
+    /// - Parameter result: The result of the completion.
+    static func mainThreadCompletion<T>(
+        _ completion: @escaping (Result<T, Error>) -> Void,
+        result: Result<T, Error>
+    ) {
+        if current == main {
+            completion(result)
+        } else {
+            DispatchQueue.main.async {
+                completion(result)
+            }
+        }
+    }
+
+}
+
+#endif
---
+++ Tests/FoundationTests/ThreadExtensionsTests.swift
@@ -0,0 +1,53 @@
+//
+//  ThreadExtensionsTests.swift
+//  SwifterSwift
+//
+//  Created by Francesco Deliro on 18/10/2019.
+//  Copyright © 2019 SwifterSwift
+//
+
+import XCTest
+@testable import SwifterSwift
+
+#if canImport(Foundation)
+import Foundation
+
+final class ThreadExtensionsTests: XCTestCase {
+
+    private enum TestError: Error {
+        case error
+    }
+
+    typealias StringCompletion = (Result<String, Error>) -> Void
+
+    func testMainThreadCompletionSuccessful() {
+        let completion: StringCompletion = { result in
+            switch result {
+            case .success(let text):
+                XCTAssertTrue(text.isEmpty)
+                XCTAssertTrue(Thread.isMainThread)
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+        }
+
+        Thread.mainThreadCompletion(completion, result: .success(""))
+    }
+
+    func testMainThreadCompletionFailure() {
+        let completion: StringCompletion = { result in
+            switch result {
+            case .success:
+                XCTFail("result should fail")
+            case .failure(let error):
+                XCTAssertTrue(error is TestError)
+                XCTAssertTrue(Thread.isMainThread)
+            }
+        }
+
+        Thread.mainThreadCompletion(completion, result: .failure(TestError.error))
+    }
+
+}
+
+#endif
