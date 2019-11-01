f20484a5844e854e61f22d2a4df647a0fa247d58
Guy Kogus <guy.kogus@gmail.com>
2019-11-01 11:05:56 +1100

    Revert test

--- Tests/UIKitTests/UITextViewExtensionsTests.swift
+++ Tests/UIKitTests/UITextViewExtensionsTests.swift
@@ -41,7 +41,7 @@ final class UITextViewExtensionsTests: XCTestCase {
         let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
         // initial setting
-        textView.bounds = CGRect(origin: .zero, size: CGSize(width: 100, height: 20))
+        textView.frame = CGRect(origin: .zero, size: CGSize(width: 100, height: 20))
         textView.font = UIFont.systemFont(ofSize: 20.0)
         textView.text = text
 
