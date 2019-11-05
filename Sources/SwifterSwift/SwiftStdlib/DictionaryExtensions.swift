87d874f921d56636892eac8c5a0739bc6cba8d7c
Morgan Dock <mmdock@users.noreply.github.com>
2019-11-05 11:43:09 -0800

    Apply suggestions from code review
    Co-Authored-By: Luciano Almeida <passos.luciano@outlook.com>

--- Sources/SwifterSwift/SwiftStdlib/DictionaryExtensions.swift
+++ Sources/SwifterSwift/SwiftStdlib/DictionaryExtensions.swift
@@ -13,11 +13,11 @@ import Foundation
 // MARK: - Methods
 public extension Dictionary {
 
-    /// SwifterSwift: Creates a Dictionary from a given sequence grouped by a given Keypath
+    /// SwifterSwift: Creates a Dictionary from a given sequence grouped by a given key path.
     ///
     /// - Parameters:
     ///   - sequence: Sequence being grouped
-    ///   - keypath: The Keypath to group by.
+    ///   - keypath: The key path to group by.
     init<S: Sequence>(grouping sequence: S, by keyPath: KeyPath<S.Element, Key>) where Value == [S.Element] {
        self.init(grouping: sequence, by: { $0[keyPath: keyPath] })
     }
