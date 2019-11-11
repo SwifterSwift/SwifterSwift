bd129b8bf4ed6a9d8965e3c0a77c0bd9bef02e81
Gheorghe Soltan <gheorghe.soltan@think-it.ro>
2019-11-11 17:30:31 +0200

    Modified UIImage extension compressed so it doens't use other extension, issue/755  🚀

--- Sources/SwifterSwift/UIKit/UIImageExtensions.swift
+++ Sources/SwifterSwift/UIKit/UIImageExtensions.swift
@@ -42,7 +42,7 @@ public extension UIImage {
     /// - Parameter quality: The quality of the resulting JPEG image, expressed as a value from 0.0 to 1.0. The value 0.0 represents the maximum compression (or lowest quality) while the value 1.0 represents the least compression (or best quality), (default is 0.5).
     /// - Returns: optional UIImage (if applicable).
     func compressed(quality: CGFloat = 0.5) -> UIImage? {
-        guard let data = compressedData(quality: quality) else { return nil }
+        guard let data = jpegData(compressionQuality: quality) else { return nil }
         return UIImage(data: data)
     }
 
