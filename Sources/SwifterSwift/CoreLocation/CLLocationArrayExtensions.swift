cf23a0edba0dfe03ce6de08319995a5f8a8e1404
Trevor Phillips <trevorcoreyphillips@gmail.com>
2020-01-25 08:51:29 -0700

    updated logic for extension

--- Sources/SwifterSwift/CoreLocation/CLLocationArrayExtensions.swift
+++ Sources/SwifterSwift/CoreLocation/CLLocationArrayExtensions.swift
@@ -14,14 +14,16 @@ public extension Array where Element: CLLocation {
 
     /// SwifterSwift: Calculates the sum of distances between each location in the array based on the curvature of the earth.
     ///
-    /// - Parameter unit: The unit of length to return the distance in.
+    /// - Parameter unitLength: The unit of length to return the distance in.
     /// - Returns: The distance in the specified unit.
     @available(iOS 10.0, tvOS 10.0, macOS 10.12, watchOS 3.0, *)
     func distance(unitLength unit: UnitLength) -> Measurement<UnitLength> {
-        let distance = self.enumerated().reduce(0.0) { (sum, position) in
-            let (index, location) = position
-            if location == self.last { return sum }
-            return sum + location.distance(from: self[index + 1])
+        guard count > 1 else {
+          return Measurement(value: 0.0, unit: unit)
+        }
+        var distance = 0.0
+        for idx in 0..<count-1 {
+            distance += self[idx].distance(from: self[idx + 1])
         }
         return Measurement(value: distance, unit: UnitLength.meters).converted(to: unit)
     }
