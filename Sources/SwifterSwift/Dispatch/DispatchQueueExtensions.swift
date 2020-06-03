c2cc0bce192f79bd0721564e67cebebdac957057
VatoKo <vkost16@freeuni.edu.ge>
2020-06-03 16:23:35 +0400

    Added convenient wrapper to asyncAfter

--- Sources/SwifterSwift/Dispatch/DispatchQueueExtensions.swift
+++ Sources/SwifterSwift/Dispatch/DispatchQueueExtensions.swift
@@ -41,6 +41,19 @@ public extension DispatchQueue {
 
         return DispatchQueue.getSpecific(key: key) != nil
     }
+    
+    /// SwifterSwift: Runs passed closure asynchronous after certain time interval
+    ///
+    /// - Parameter delay: The time inverval after which the closure will run.
+    /// - Parameter qos: Quality of service at which the work item should be executed.
+    /// - Parameter flags: Flags that control the execution environment of the work item.
+    /// - Parameter work: The closure to run after certain time interval.
+    func asyncAfter(delay: Double,
+                    qos: DispatchQoS = .unspecified,
+                    flags: DispatchWorkItemFlags = [],
+                    execute work: @escaping @convention(block) () -> Void) {
+        asyncAfter(deadline: .now() + delay, qos: qos, flags: flags, execute: work)
+    }
 
 }
 
