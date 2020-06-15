92e6eca8c051401b1172aaef5cf6f2e2890cbcd0
phil zhang <goo.gle@foxmail.com>
2020-06-15 18:59:23 +0800

    Added `topMost ` extensions for UIViewController.

--- Sources/SwifterSwift/UIKit/UIViewControllerExtensions.swift
+++ Sources/SwifterSwift/UIKit/UIViewControllerExtensions.swift
@@ -126,3 +126,62 @@ public extension UIViewController {
 }
 
 #endif
+
+/// copy from: https://github.com/devxoul/URLNavigator/blob/master/Sources/URLNavigator/UIViewController%2BTopMostViewController.swift
+#if canImport(UIKit) && !os(watchOS)
+public extension UIViewController {
+  private class var sharedApplication: UIApplication? {
+    let selector = NSSelectorFromString("sharedApplication")
+    return UIApplication.perform(selector)?.takeUnretainedValue() as? UIApplication
+  }
+
+  /// Returns the current application's top most view controller.
+  class var topMost: UIViewController? {
+    guard let currentWindows = self.sharedApplication?.windows else { return nil }
+    var rootViewController: UIViewController?
+    for window in currentWindows {
+      if let windowRootViewController = window.rootViewController, window.isKeyWindow {
+        rootViewController = windowRootViewController
+        break
+      }
+    }
+
+    return self.topMost(of: rootViewController)
+  }
+
+  /// Returns the top most view controller from given view controller's stack.
+  class func topMost(of viewController: UIViewController?) -> UIViewController? {
+    // presented view controller
+    if let presentedViewController = viewController?.presentedViewController {
+      return self.topMost(of: presentedViewController)
+    }
+
+    // UITabBarController
+    if let tabBarController = viewController as? UITabBarController,
+      let selectedViewController = tabBarController.selectedViewController {
+      return self.topMost(of: selectedViewController)
+    }
+
+    // UINavigationController
+    if let navigationController = viewController as? UINavigationController,
+      let visibleViewController = navigationController.visibleViewController {
+      return self.topMost(of: visibleViewController)
+    }
+
+    // UIPageController
+    if let pageViewController = viewController as? UIPageViewController,
+      pageViewController.viewControllers?.count == 1 {
+      return self.topMost(of: pageViewController.viewControllers?.first)
+    }
+
+    // child view controller
+    for subview in viewController?.view?.subviews ?? [] {
+      if let childViewController = subview.next as? UIViewController {
+        return self.topMost(of: childViewController)
+      }
+    }
+
+    return viewController
+  }
+}
+#endif
