e19fe86b3fff7a6bb754414352b0262b518128aa
Gilbert Lo <lhygilbert@gmail.com>
2020-07-03 20:09:48 -0700

    Use .compare to avoid using an arbitrary unit

--- Sources/SwifterSwift/HealthKit/HKActivitySummaryExtensions.swift
+++ Sources/SwifterSwift/HealthKit/HKActivitySummaryExtensions.swift
@@ -15,26 +15,17 @@ public extension HKActivitySummary {
 
     /// SwifterSwift: Check if stand goal is met
     var isStandGoalMet: Bool {
-        let unit = HKUnit.count()
-        let standHours = appleStandHours.doubleValue(for: unit)
-        let goal = appleStandHoursGoal.doubleValue(for: unit)
-        return standHours >= goal
+        return appleStandHoursGoal.compare(appleStandHours) != .orderedDescending
     }
 
     /// SwifterSwift: Check if exercise time goal is met
     var isExerciseGoalMet: Bool {
-        let unit = HKUnit.minute()
-        let exerciseTime = appleExerciseTime.doubleValue(for: unit)
-        let goal = appleExerciseTimeGoal.doubleValue(for: unit)
-        return exerciseTime >= goal
+        return appleExerciseTimeGoal.compare(appleExerciseTime) != .orderedDescending
     }
 
     /// SwifterSwift: Check if active energy goal is met
     var isEnergyGoalMet: Bool {
-        let unit = HKUnit.jouleUnit(with: .kilo)
-        let energyBurned = activeEnergyBurned.doubleValue(for: unit)
-        let goal = activeEnergyBurnedGoal.doubleValue(for: unit)
-        return energyBurned >= goal
+        return activeEnergyBurnedGoal.compare(activeEnergyBurned) != .orderedDescending
     }
 
 }
