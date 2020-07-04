11a30e3c72ea1b1549e6c24dfe61c8352e4e6447
Gilbert Lo <lhygilbert@gmail.com>
2020-07-03 17:54:01 -0700

    Add HKActivitySummaryExtensions

---
+++ Sources/SwifterSwift/HealthKit/HKActivitySummaryExtensions.swift
@@ -0,0 +1,41 @@
+//
+//  HKActivitySummaryExtensions.swift
+//  SwifterSwift
+//
+//  Created by Gilbert Lo on 7/3/20.
+//  Copyright © 2020 SwifterSwift
+//
+
+#if canImport(HealthKit)
+import HealthKit
+
+// MARK: - Properties
+public extension HKActivitySummary {
+
+    /// SwifterSwift: Check if stand goal is met
+    var isStandGoalMet: Bool {
+        let unit = HKUnit.count()
+        let standHours = appleStandHours.doubleValue(for: unit)
+        let goal = appleStandHoursGoal.doubleValue(for: unit)
+        return standHours >= goal
+    }
+
+    /// SwifterSwift: Check if exercise time goal is met
+    var isExerciseGoalMet: Bool {
+        let unit = HKUnit.minute()
+        let exerciseTime = appleExerciseTime.doubleValue(for: unit)
+        let goal = appleExerciseTimeGoal.doubleValue(for: unit)
+        return exerciseTime >= goal
+    }
+
+    /// SwifterSwift: Check if active energy goal is met
+    var isEnergyGoalMet: Bool {
+        let unit = HKUnit.jouleUnit(with: .kilo)
+        let energyBurned = activeEnergyBurned.doubleValue(for: unit)
+        let goal = activeEnergyBurnedGoal.doubleValue(for: unit)
+        return energyBurned >= goal
+    }
+
+}
+
+#endif
