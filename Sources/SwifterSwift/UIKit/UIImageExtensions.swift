9193e045f30e6ef01db188618accb0858abd23d8
Adam Doppelt <amd@gurge.com>
2020-08-21 09:24:31 -0700

    Update Sources/SwifterSwift/UIKit/UIImageExtensions.swift
    Co-authored-by: Guy Kogus <guy.kogus@gmail.com>

--- Sources/SwifterSwift/UIKit/UIImageExtensions.swift
+++ Sources/SwifterSwift/UIKit/UIImageExtensions.swift
@@ -37,9 +37,7 @@ public extension UIImage {
     func averageColor() -> UIColor? {
         // https://stackoverflow.com/questions/26330924
         // note: self.ciImage appears to always be nil
-        guard let ciImage = CIImage(image: self) else {
-            return nil
-        }
+        guard let ciImage = ciImage ?? CIImage(image: self) else { return nil }
 
         // CIAreaAverage returns a single-pixel image that contains the average color for a given region of an image.
         let parameters = [kCIInputImageKey: ciImage, kCIInputExtentKey: CIVector(cgRect: ciImage.extent)]
