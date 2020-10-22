cf1ac9ef4e4857b40d12269be1006e8dca370e92
Henry <storch99@gmx.de>
2020-10-22 12:57:19 +0200

    Update Tests/SceneKitTests/SCNVector3ExtensionsTests.swift
    Co-authored-by: Guy Kogus <guy.kogus@gmail.com>

--- Tests/SceneKitTests/SCNVector3ExtensionsTests.swift
+++ Tests/SceneKitTests/SCNVector3ExtensionsTests.swift
@@ -25,7 +25,7 @@ final class SceneKitTests: XCTestCase {
         
         let vector4 = SCNVector3(4, 4, 2)
         let v4Norm = vector4.normalized
-        XCTAssertEqual(v4Norm.x, 4/6)
+        XCTAssertEqual(v4Norm.x, 4 / 6)
         XCTAssertEqual(v4Norm.x, v4Norm.y)
         XCTAssertEqual(v4Norm.z, 2/6)
     }
