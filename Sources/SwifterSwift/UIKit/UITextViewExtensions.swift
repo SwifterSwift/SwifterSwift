83f39af9953bd77b6c043e762b734502f82f701a
Sebastian Kreft <sebastian@copilot.money>
2020-11-04 08:03:08 -0300

    chore: reenable rule legacy_constructor
    The change was autocorrected using swiftlint itself.

--- Sources/SwifterSwift/UIKit/UITextViewExtensions.swift
+++ Sources/SwifterSwift/UIKit/UITextViewExtensions.swift
@@ -14,15 +14,13 @@ public extension UITextView {
 
     /// SwifterSwift: Scroll to the bottom of text view
     func scrollToBottom() {
-        // swiftlint:disable:next legacy_constructor
-        let range = NSMakeRange((text as NSString).length - 1, 1)
+        let range = NSRange(location: (text as NSString).length - 1, length: 1)
         scrollRangeToVisible(range)
     }
 
     /// SwifterSwift: Scroll to the top of text view
     func scrollToTop() {
-        // swiftlint:disable:next legacy_constructor
-        let range = NSMakeRange(0, 1)
+        let range = NSRange(location: 0, length: 1)
         scrollRangeToVisible(range)
     }
 
