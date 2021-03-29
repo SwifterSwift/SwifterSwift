ad23cdfca771e4067165b12256b4033efcaa04aa
Yonat Sharon <yonat@ootips.org>
2021-03-29 04:13:15 +0300

    Add `beginRefreshing(animated:, sendAction:)` that works inside any `UIScrollView` and not only `UITableView`

--- Sources/SwifterSwift/UIKit/UIRefreshControlExtensions.swift
+++ Sources/SwifterSwift/UIKit/UIRefreshControlExtensions.swift
@@ -6,7 +6,7 @@ import UIKit
 // MARK: - Methods
 
 public extension UIRefreshControl {
-    /// SwifterSwift: Programatically begin refresh control inside of UITableView.
+    /// SwifterSwift: Programmatically begin refresh control inside of UITableView.
     ///
     /// - Parameters:
     ///   - tableView: UITableView instance, inside which the refresh control is contained.
@@ -16,9 +16,23 @@ public extension UIRefreshControl {
         // https://stackoverflow.com/questions/14718850/14719658#14719658
         assert(superview == tableView, "Refresh control does not belong to the receiving table view")
 
+        beginRefreshing(animated: animated, sendAction: sendAction)
+    }
+
+    /// SwifterSwift: Programmatically begin refresh control inside of UIScrollView.
+    ///
+    /// - Parameters:
+    ///   - animated: Boolean, indicates that is the content offset changing should be animated or not.
+    ///   - sendAction: Boolean, indicates that should it fire sendActions method for valueChanged UIControlEvents.
+    func beginRefreshing(animated: Bool, sendAction: Bool = false) {
+        // https://stackoverflow.com/questions/14718850/14719658#14719658
+        guard let scrollView = superview as? UIScrollView else {
+            assert(false, "Refresh control does not belong to a scroll view")
+        }
+
         beginRefreshing()
         let offsetPoint = CGPoint(x: 0, y: -frame.height)
-        tableView.setContentOffset(offsetPoint, animated: animated)
+        scrollView.setContentOffset(offsetPoint, animated: animated)
 
         if sendAction {
             sendActions(for: .valueChanged)
