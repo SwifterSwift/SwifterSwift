57ae92f89ff56c7ac044c765bc72779a6908a79e
Randhir kumar <randhir_kumar@intuit.com>
2021-06-26 21:08:50 +0530

    SwifterSwift: Added a os check.

--- Sources/SwifterSwift/UIKit/UITextFieldExtensions.swift
+++ Sources/SwifterSwift/UIKit/UITextFieldExtensions.swift
@@ -177,6 +177,7 @@ public extension UITextField {
     /// - Parameters:
     ///   - items: The items to present in the toolbar.
     ///   - height: The height of the toolbar.
+    #if os(iOS)
     @discardableResult
     func addToolbar(items: [UIBarButtonItem]?, height: CGFloat = 44) -> UIToolbar {
         let toolBar = UIToolbar(frame: CGRect(x: 0.0, y: 0.0, width: UIScreen.main.bounds.size.width, height: height))
@@ -184,6 +185,7 @@ public extension UITextField {
         inputAccessoryView = toolBar
         return toolBar
     }
+    #endif
 }
 
 #endif
