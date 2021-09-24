2c686001fe89d51b54513fa2a8921a081944906c
czn <443623074@qq.com>
2021-09-24 12:21:16 +0800

    IOS15 have failed on the old way, USES the UINavigationBarAppearance to set the UINavigationBar

--- Sources/SwifterSwift/UIKit/UINavigationBarExtensions.swift
+++ Sources/SwifterSwift/UIKit/UINavigationBarExtensions.swift
@@ -15,7 +15,17 @@ public extension UINavigationBar {
         var attrs = [NSAttributedString.Key: Any]()
         attrs[.font] = font
         attrs[.foregroundColor] = color
-        titleTextAttributes = attrs
+        if #available(iOS 13.0, *) {
+            let standardApp = standardAppearance
+            standardApp.titleTextAttributes = attrs
+            standardAppearance = standardApp
+            if let scrollApp = scrollEdgeAppearance {
+                scrollApp.titleTextAttributes = attrs
+                scrollEdgeAppearance = scrollApp
+            }
+        }else{
+            titleTextAttributes = attrs
+        }
     }
 
     /// SwifterSwift: Make navigation bar transparent.
@@ -23,12 +33,17 @@ public extension UINavigationBar {
     /// - Parameter tint: tint color (default is .white).
     func makeTransparent(withTint tint: UIColor = .white) {
         isTranslucent = true
-        backgroundColor = .clear
-        barTintColor = .clear
-        setBackgroundImage(UIImage(), for: .default)
-        tintColor = tint
-        titleTextAttributes = [.foregroundColor: tint]
-        shadowImage = UIImage()
+        if #available(iOS 13.0, *) {
+            standardAppearance.configureWithTransparentBackground()
+            scrollEdgeAppearance?.configureWithTransparentBackground()
+        }else{
+            backgroundColor = .clear
+            barTintColor = .clear
+            setBackgroundImage(UIImage(), for: .default)
+            tintColor = tint
+            titleTextAttributes = [.foregroundColor: tint]
+            shadowImage = UIImage()
+        }
     }
 
     /// SwifterSwift: Set navigationBar background and text colors.
@@ -38,11 +53,24 @@ public extension UINavigationBar {
     ///   - text: text color.
     func setColors(background: UIColor, text: UIColor) {
         isTranslucent = false
-        backgroundColor = background
         barTintColor = background
-        setBackgroundImage(UIImage(), for: .default)
-        tintColor = text
-        titleTextAttributes = [.foregroundColor: text]
+        if #available(iOS 13.0, *) {
+            let standardApp = standardAppearance
+            standardApp.backgroundColor = background
+            standardApp.backgroundImage = UIImage()
+            standardApp.titleTextAttributes = [.foregroundColor: text]
+            standardAppearance = standardApp
+            if let scrollApp = scrollEdgeAppearance {
+                scrollApp.backgroundColor = background
+                scrollApp.backgroundImage = UIImage()
+                scrollApp.titleTextAttributes = [.foregroundColor: text]
+            }
+        } else{
+            backgroundColor = background
+            setBackgroundImage(UIImage(), for: .default)
+            tintColor = text
+            titleTextAttributes = [.foregroundColor: text]
+        }
     }
 }
 
