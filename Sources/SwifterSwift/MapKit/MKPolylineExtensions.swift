dc907376fb7a5191d08ec82599cab04a45842e59
Rizwan Mohamed Ibrahim <rizwankce@users.noreply.github.com>
2021-10-08 08:41:29 +0530

    Add `MKMultiPoint` extensions (#990)
    * Add `MKMultiPoint` extensions
    
    * Remove duplicate tests for MKPolyLine coordinates

--- Sources/SwifterSwift/MapKit/MKPolylineExtensions.swift
+++ Sources/SwifterSwift/MapKit/MKPolylineExtensions.swift
@@ -16,16 +16,4 @@ public extension MKPolyline {
     }
 }
 
-// MARK: - Properties
-
-@available(tvOS 9.2, *)
-public extension MKPolyline {
-    /// SwifterSwift: Return an Array of coordinates representing the provided polyline.
-    var coordinates: [CLLocationCoordinate2D] {
-        var coords = [CLLocationCoordinate2D](repeating: kCLLocationCoordinate2DInvalid, count: pointCount)
-        getCoordinates(&coords, range: NSRange(location: 0, length: pointCount))
-        return coords
-    }
-}
-
 #endif
