019e8c8a1268940dd7e8667b4141ff3299dd558d
Can Yoldas <can.yoldas0@outlook.com>
2022-08-03 22:26:31 +0300

    added generic handler extension

---
+++ Sources/SwifterSwift/Shared/CompletionExtensions.swift
@@ -0,0 +1,10 @@
+//
+//  CompletionExtensions.swift
+//  SwifterSwift
+//
+//  Created by Can Yoldaş on 3.08.2022.
+//  Copyright © 2022 SwifterSwift. All rights reserved.
+//
+
+/// Typealias for defining handlers in generic way.
+public typealias GenericHandler<T> = (T) -> Void
