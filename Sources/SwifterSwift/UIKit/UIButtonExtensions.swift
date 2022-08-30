cf8e427196b499b3d64886a049a4325c89857fe5
TwizzyIndy <twizzy.indy@gmail.com>
2022-08-30 23:18:14 +0630

    use UIGraphicsBeginImageContext for older ver.

--- Sources/SwifterSwift/UIKit/UIButtonExtensions.swift
+++ Sources/SwifterSwift/UIKit/UIButtonExtensions.swift
@@ -187,12 +187,24 @@ public extension UIButton {
     func setBackgroundColor(color: UIColor, forState: UIControl.State) {
         clipsToBounds = true  // maintain corner radius
         
-        let colorImage = UIGraphicsImageRenderer(size: CGSize(width: 1, height: 1)).image { context in
-            color.setFill()
-            context.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
-            draw(.zero)
+        if #available(iOS 10.0, tvOS 10.0, *) {
+            let colorImage = UIGraphicsImageRenderer(size: CGSize(width: 1, height: 1)).image { context in
+                color.setFill()
+                context.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+                draw(.zero)
+            }
+            setBackgroundImage(colorImage, for: forState)
+            return
         }
-        setBackgroundImage(colorImage, for: forState)
+        
+        UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
+        if let context = UIGraphicsGetCurrentContext() {
+            context.setFillColor(color.cgColor)
+            context.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+            let colorImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            setBackgroundImage(colorImage, for: forState)
+        }
     }
 }
 
