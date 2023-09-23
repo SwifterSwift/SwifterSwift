40269febd11f49939b9f7857d49d384fd0f3826e
Roman Podymov <podymfrombryansk@yandex.com>
2023-09-23 15:54:52 +0200

    Combine.Future Extensions

---
+++ Sources/SwifterSwift/Combine/FutureExtensions.swift
@@ -0,0 +1,36 @@
+// FutureExtensions.swift - Copyright 2023 SwifterSwift
+
+#if canImport(Combine) && canImport(_Concurrency)
+import Combine
+
+// MARK: - Methods
+
+@available(iOS 13.00, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+public extension Future where Failure == Error {
+    convenience init(asyncFunc: @escaping () async throws -> Output) {
+        self.init { promise in
+            Task {
+                do {
+                    let result = try await asyncFunc()
+                    promise(.success(result))
+                } catch {
+                    promise(.failure(error))
+                }
+            }
+        }
+    }
+}
+
+@available(iOS 13.00, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+public extension Future where Failure == Never {
+    convenience init(asyncFunc: @escaping () async -> Output) {
+        self.init { promise in
+            Task {
+                let result = await asyncFunc()
+                promise(.success(result))
+            }
+        }
+    }
+}
+
+#endif
