08bd23a34a474de0576ac8b722d1e223e7c0b68b
Roman Podymov <podymfrombryansk@yandex.com>
2023-09-23 20:11:48 +0200

    Docs

--- Sources/SwifterSwift/Combine/FutureExtensions.swift
+++ Sources/SwifterSwift/Combine/FutureExtensions.swift
@@ -7,6 +7,8 @@ import Combine
 
 @available(iOS 13.00, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
 public extension Future where Failure == Error {
+    /// Create a `Future` from an `async` function
+    /// - Parameter asyncFunc: `async` function
     convenience init(asyncFunc: @escaping () async throws -> Output) {
         self.init { promise in
             Task {
@@ -23,6 +25,8 @@ public extension Future where Failure == Error {
 
 @available(iOS 13.00, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
 public extension Future where Failure == Never {
+    /// Create a `Future` from an `async` throwing function
+    /// - Parameter asyncFunc: `async` throwing function
     convenience init(asyncFunc: @escaping () async -> Output) {
         self.init { promise in
             Task {
