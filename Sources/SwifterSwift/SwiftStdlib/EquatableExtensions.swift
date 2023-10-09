3ad01102d352a9af01395ef4f71289323c0e11bf
Adam Wienconek <adam.wienconek@new-gravity.com>
2023-10-09 13:31:40 +0200

    Added similar extension for all Equatable types

--- Sources/SwifterSwift/SwiftStdlib/EquatableExtensions.swift
+++ Sources/SwifterSwift/SwiftStdlib/EquatableExtensions.swift
@@ -1,36 +1,39 @@
-// CaseIterableExtensions.swift - Copyright 2023 SwifterSwift
+// EquatableExtensions.swift - Copyright 2023 SwifterSwift
 
 import Foundation
 
-public extension CaseIterable where Self: Equatable, AllCases == [Self] {
+public extension Equatable {
     
-    /// SwifterSwift: Sets the value to next available case from `allCases` array.
+    /// SwifterSwift: Sets the value to next available value from given array.
     ///
-    ///     enum ShuffleMode: CaseIterable {
+    ///     enum ShuffleMode {
     ///        case off, songs, albums
     ///     }
     ///
+    ///     let values: [ShuffleMode] = [.off, .songs, .albums]
+    ///
     ///     var mode: ShuffleMode = .off
-    ///     mode.toggle() // .songs
+    ///     mode.toggle(between: values) // .songs
     ///
     ///     var mode: ShuffleMode = .albums
-    ///     mode.toggle() // .off
+    ///     mode.toggle(between: values) // .off
     ///
     ///     var mode: ShuffleMode = .albums
-    ///     mode.toggle(cycle: false) // .albums
+    ///     mode.toggle(between: values, cycle: false) // .albums
     ///
     ///     var mode: ShuffleMode = .albums
     ///     mode.toggle(between: [.songs, .albums]) // .songs
     ///
     /// - Parameters:
+    ///    - cases: Array of values that can be assigned to the caller.
     ///    - cycle: Indicates whether the value should be modified if it's already at the end of `values` array.
     ///
     mutating func toggle(
+        between values: [Self],
         cycle: Bool = false
     ) {
-        let values = Self.allCases
         guard var index = values.firstIndex(of: self) else {
-            fatalError("Case \(self) must be included in `values` array.")
+            fatalError("Value \(self) must be included in `values` array.")
         }
         if index < (values.count - 1) {
             index += 1
