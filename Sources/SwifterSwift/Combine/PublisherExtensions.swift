4f234de92aaa5e15248cd32a3b5f00ce72adb75e
Heecheon Park <heecheon92@gmail.com>
2023-10-11 20:08:07 +0900

    Update Sources/SwifterSwift/Combine/PublisherExtensions.swift
    Co-authored-by: Guy Kogus <guy.kogus@gmail.com>

--- Sources/SwifterSwift/Combine/PublisherExtensions.swift
+++ Sources/SwifterSwift/Combine/PublisherExtensions.swift
@@ -7,8 +7,7 @@
 //
 
 // swiftlint:disable large_tuple
-#if canImport(Foundation) && canImport(Combine)
-import Foundation
+#if canImport(Combine)
 import Combine
 
 @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
