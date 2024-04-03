ff901866cbc18b36dadf800e20053ac0e3081917
Martônio Júnior <martoniojunior@hotmail.com>
2024-04-03 11:34:15 -0300

    fix: Added missing bracket to `contains`

--- Sources/SwifterSwift/SwiftStdlib/SequenceExtensions.swift
+++ Sources/SwifterSwift/SwiftStdlib/SequenceExtensions.swift
@@ -343,7 +343,7 @@ public extension Sequence where Element: Equatable {
     /// - Parameter elements: array of elements to check.
     /// - Returns: true if sequence contains all given items.
     /// - Complexity: _O(m·n)_, where _m_ is the length of `elements` and _n_ is the length of this sequence.
-    func contains<S>(_ elements: S) -> Bool where S: Sequence, Element == S.Element
+    func contains<S>(_ elements: S) -> Bool where S: Sequence, Element == S.Element {
         return elements.allSatisfy { contains($0) }
     }
 }
